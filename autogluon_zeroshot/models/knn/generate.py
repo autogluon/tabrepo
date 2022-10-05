f42aa22f5c7cbd68d9ed966061b43582a0b1bf2c
Innixma <neerick@amazon.com>
2022-10-05 16:05:32 -0700

    update knn config

--- autogluon_zeroshot/models/knn/generate.py
+++ autogluon_zeroshot/models/knn/generate.py
@@ -9,12 +9,12 @@ manual_configs = [
     {'weights': 'distance'},
 ]
 search_space = {
-    'n_neighbors': Categorical(3, 4, 5, 7, 9, 11, 13, 15, 20, 30, 50),
+    'n_neighbors': Categorical(3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 20, 30, 50),
     'weights': Categorical('uniform', 'distance'),
     'p': Categorical(2, 1),
 }
 
 
-def generate_configs_knn(num_random_configs=30):
+def generate_configs_knn(num_random_configs=50):
     config_generator = ConfigGenerator(name=name, manual_configs=manual_configs, search_space=search_space)
     return config_generator.generate_all_configs(num_random_configs=num_random_configs)
