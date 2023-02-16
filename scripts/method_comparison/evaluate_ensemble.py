add option to average cross fold when evaluating ensemble configurations, fix print in catchtime

scripts/method_comparison/evaluate_ensemble.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from typing import List
 
 import numpy as np
+from sklearn.model_selection import KFold
 from syne_tune import Reporter
 
 from autogluon_zeroshot.contexts.context_2022_10_13 import load_context_2022_10_13
@@ -18,30 +19,59 @@ from autogluon_zeroshot.utils import catchtime
 
 
 def evaluate_ensemble(
-        configs: List[dict],
+        configs: List[str],
         train_datasets: List[str],
         test_datasets: List[str],
         ensemble_size: int,
         num_folds: int = 10,
         backend: str = "native",
         bag: bool = False,
+        n_splits: int = None,
 ):
-    print(f"Evaluating backend/bag/ensemble_size/num_folds:{backend}/{bag}/{ensemble_size}/{num_folds}")
+    """
+    :param configs: model configurations to evaluate, for instance ['CatBoost_r2_BAG_L1', 'LightGBM_r12_BAG_L1']
+    :param train_datasets: dataset with fold to evaluate for instance ['359979_0', '359979_1', '359979_2', ...]
+    :param test_datasets:
+    :param ensemble_size: number of ensembles to compute in greedy caruana.
+    :param num_folds: number of folds to consider in the datasets, used to quickly check things work and only works with n_splits=None.
+    :param backend: native or ray
+    :param bag: whether to load configurations evaluated with bagging or without
+    :param n_splits: The train error is estimated by taking the average score on `n_splits` subsets of `train_datasets`.
+    If the value is None, then all datasets are used.
+    :return:
+    """
+    assert backend in ['native', 'ray']
+    print(f"Evaluating backend/bag/ensemble_size/num_folds:{backend}/{bag}/{ensemble_size}/{num_folds}/{n_splits}")
     load_ctx = load_context_2022_12_11_bag if bag else load_context_2022_10_13
 
     zsc, configs_full, zeroshot_pred_proba, zeroshot_gt = load_ctx(
         load_zeroshot_pred_proba=True, lazy_format=True,
     )
-    train_scorer = EnsembleSelectionConfigScorer.from_zsc(
-        datasets=train_datasets,
-        zeroshot_simulator_context=zsc,
-        zeroshot_gt=zeroshot_gt,
-        zeroshot_pred_proba=zeroshot_pred_proba,
-        ensemble_size=ensemble_size,
-        max_fold=num_folds,
-        backend=backend,
-    )
-    train_error = train_scorer.score(configs)
+    if n_splits is not None:
+        datasets = np.array(train_datasets)
+        np.random.shuffle(datasets)
+        kf = KFold(n_splits=n_splits, random_state=0, shuffle=True)
+        splits = kf.split(datasets)
+        train_datasets_splits = [
+            [datasets[i] for i in train_indices]
+            for (train_indices, _) in splits
+        ]
+    else:
+        train_datasets_splits = [train_datasets]
+    # compute errors on all splits and then take average to estimate the final performance
+    train_errors = []
+    for datasets in train_datasets_splits:
+        train_score = EnsembleSelectionConfigScorer.from_zsc(
+            datasets=datasets,
+            zeroshot_simulator_context=zsc,
+            zeroshot_gt=zeroshot_gt,
+            zeroshot_pred_proba=zeroshot_pred_proba,
+            ensemble_size=ensemble_size,
+            max_fold=num_folds,
+            backend=backend,
+        ).score(configs)
+        train_errors.append(train_score)
+    train_error = np.mean(train_errors)
     if len(test_datasets) > 0:
         test_scorer = EnsembleSelectionConfigScorer.from_zsc(
             datasets=test_datasets,