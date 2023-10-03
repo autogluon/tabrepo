2bd2e39800b1763887c0294b435f67d49c667590
innixma <innixma@gmail.com>
2023-10-03 00:51:57 +0000

    Add TaskMetadata and EnsembleScorer

--- autogluon_zeroshot/simulation/ensemble_selection_config_scorer.py
+++ autogluon_zeroshot/simulation/ensemble_selection_config_scorer.py
@@ -1,4 +1,7 @@
-from typing import Dict, List, Optional, Tuple, Union
+from __future__ import annotations
+
+import copy
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 import numpy as np
 import ray
@@ -8,7 +11,7 @@ from autogluon.core.models.greedy_ensemble.ensemble_selection import EnsembleSel
 from .configuration_list_scorer import ConfigurationListScorer
 
 from .simulation_context import ZeroshotSimulatorContext
-from .simulation_context import TabularModelPredictions
+from .simulation_context import TabularModelPredictions, TabularPicklePredictions
 from ..utils.rank_utils import RankScorer
 from ..metrics import _fast_log_loss, _fast_roc_auc
 
@@ -17,6 +20,133 @@ from ..metrics import _fast_log_loss, _fast_roc_auc
 def compute_error_ray(config_scorer, configs: List[str], task: str) -> (float, dict):
     error, ensemble_weights = config_scorer.run_task(task=task, models=configs)
     return error, ensemble_weights
+
+
+class TaskMetadata:
+    def __init__(self, task_metadata: Dict[str, Any]):
+        self.task_metadata = task_metadata
+
+    def get_ground_truth(self):
+        y_val = self.task_metadata["y_val"].to_numpy()
+        y_test = self.task_metadata["y_test"].fillna(-1).to_numpy()
+        return y_val, y_test
+
+    def get_metric(self) -> Scorer:
+        metric_name = self.task_metadata["eval_metric"]
+        problem_type = self.task_metadata["problem_type"]
+        return self._get_metric_from_name(metric_name=metric_name, problem_type=problem_type)
+
+    def _get_metric_from_name(self, metric_name: str, problem_type: str) -> Scorer:
+        return get_metric(metric=metric_name, problem_type=problem_type)
+
+    @property
+    def problem_type(self):
+        return self.task_metadata["problem_type"]
+
+    @property
+    def metric_name(self):
+        return self.task_metadata["eval_metric"]
+
+
+class EnsembleScorer:
+    def __init__(self,
+                 zeroshot_pp: TabularModelPredictions | dict,
+                 zeroshot_gt: Dict[str, Dict[int, Dict[str, Any]]],
+                 ensemble_method: callable = EnsembleSelection,
+                 ensemble_method_kwargs: dict = None,
+                 proxy_fit_metric_map: dict = None,
+                 use_fast_metrics: bool = True):
+        if isinstance(zeroshot_pp, dict):
+            zeroshot_pp = TabularPicklePredictions(pred_dict=zeroshot_pp)
+        if proxy_fit_metric_map is None:
+            proxy_fit_metric_map = dict()
+        if ensemble_method_kwargs is None:
+            ensemble_method_kwargs = dict()
+        ensemble_method_kwargs = copy.deepcopy(ensemble_method_kwargs)
+        if "ensemble_size" not in ensemble_method_kwargs:
+            ensemble_method_kwargs["ensemble_size"] = 100
+        self.ensemble_method: callable = ensemble_method
+        self.ensemble_method_kwargs = ensemble_method_kwargs
+        self.zeroshot_pp: TabularModelPredictions = zeroshot_pp
+        self.zeroshot_gt = zeroshot_gt
+        self.proxy_fit_metric_map = proxy_fit_metric_map
+        self.use_fast_metrics = use_fast_metrics
+
+    def _get_metric_from_name(self, metric_name: str, problem_type: str) -> Scorer:
+        if self.use_fast_metrics:
+            return self._get_fast_metric_if_exist(metric_name=metric_name, problem_type=problem_type)
+        else:
+            return get_metric(metric=metric_name, problem_type=problem_type)
+
+    def _get_fast_metric_if_exist(self, metric_name: str, problem_type: str) -> Scorer:
+        """
+        # TODO: Add docstring
+        # TODO: Consider making this more standardized.
+        #  Currently fast_log_loss needs a bit of special preprocessing of the data and isn't a straightforward replace.
+        """
+        if metric_name == 'log_loss':
+            # TODO: Can be even faster if we transform pred_val and pred_test
+            #  as a preprocessing step to TabularModelPredictions.
+            #  This would avoid ever having to pay the preprocessing time cost, and would massively reduce memory usage.
+            eval_metric = _fast_log_loss.fast_log_loss
+        elif metric_name == 'roc_auc':
+            eval_metric = _fast_roc_auc.fast_roc_auc_cpp
+        else:
+            eval_metric = get_metric(metric=metric_name, problem_type=problem_type)
+        return eval_metric
+
+    def get_preds_from_models(self, dataset: str, fold: int, models: List[str]):
+        pred_val, pred_test = self.zeroshot_pp.predict(dataset=dataset, fold=fold, splits=['val', 'test'], models=models)
+        return pred_val, pred_test
+
+    def get_task_metadata(self, dataset: str, fold: int) -> TaskMetadata:
+        return TaskMetadata(self.zeroshot_gt[dataset][fold])
+
+    def run_task(self, dataset: str, fold: int, models: List[str]) -> Tuple[float, np.array]:
+        task_metadata = self.get_task_metadata(dataset=dataset, fold=fold)
+
+        metric_name = task_metadata.metric_name
+        problem_type = task_metadata.problem_type
+
+        y_val, y_test = task_metadata.get_ground_truth()
+        pred_val, pred_test = self.get_preds_from_models(dataset=dataset, fold=fold, models=models)
+
+        if problem_type == 'binary':
+            # Force binary prediction probabilities to 1 dimensional prediction probabilites of the positive class
+            # if it is in multiclass format
+            if len(pred_val.shape) == 3:
+                pred_val = pred_val[:, :, 1]
+            if len(pred_test.shape) == 3:
+                pred_test = pred_test[:, :, 1]
+
+        fit_metric_name = self.proxy_fit_metric_map.get(metric_name, metric_name)
+
+        eval_metric = self._get_metric_from_name(metric_name=metric_name, problem_type=problem_type)
+        fit_eval_metric = self._get_metric_from_name(metric_name=fit_metric_name, problem_type=problem_type)
+
+        if hasattr(fit_eval_metric, 'preprocess_bulk'):
+            y_val, pred_val = fit_eval_metric.preprocess_bulk(y_val, pred_val)
+
+        weighted_ensemble = self.ensemble_method(
+            problem_type=problem_type,
+            metric=fit_eval_metric,
+            **self.ensemble_method_kwargs,
+        )
+
+        weighted_ensemble.fit(predictions=pred_val, labels=y_val)
+
+        if hasattr(eval_metric, 'preprocess_bulk'):
+            y_test, pred_test = eval_metric.preprocess_bulk(y_test, pred_test)
+
+        if eval_metric.needs_pred:
+            y_test_pred = weighted_ensemble.predict(pred_test)
+        else:
+            y_test_pred = weighted_ensemble.predict_proba(pred_test)
+        err = eval_metric.error(y_test, y_test_pred)
+
+        ensemble_weights: np.array = weighted_ensemble.weights_
+
+        return err, ensemble_weights
 
 
 # FIXME: Add temperature scaling!!
@@ -70,6 +200,16 @@ class EnsembleSelectionConfigScorer(ConfigurationListScorer):
             proxy_fit_metric_map = {'roc_auc': 'log_loss'}  # log_loss is fast to compute and a good proxy for roc_auc
         self.proxy_fit_metric_map = proxy_fit_metric_map
 
+        ensemble_selection_kwargs = copy.deepcopy(ensemble_selection_kwargs)
+        ensemble_selection_kwargs["ensemble_size"] = ensemble_size
+        self.ensemble_scorer = EnsembleScorer(
+            zeroshot_pp=zeroshot_pred_proba,
+            zeroshot_gt=zeroshot_gt,
+            ensemble_method_kwargs=ensemble_selection_kwargs,
+            proxy_fit_metric_map=proxy_fit_metric_map,
+            use_fast_metrics=use_fast_metrics,
+        )
+
     @classmethod
     def from_zsc(cls, zeroshot_simulator_context: ZeroshotSimulatorContext, **kwargs):
         if 'datasets' not in kwargs:
@@ -81,85 +221,10 @@ class EnsembleSelectionConfigScorer(ConfigurationListScorer):
             **kwargs,
         )
 
-    def _get_metric_from_name(self, metric_name: str, problem_type: str) -> Scorer:
-        if self.use_fast_metrics:
-            return self._get_fast_metric_if_exist(metric_name=metric_name, problem_type=problem_type)
-        else:
-            return get_metric(metric=metric_name, problem_type=problem_type)
-
-    def _get_fast_metric_if_exist(self, metric_name: str, problem_type: str) -> Scorer:
-        """
-        # TODO: Add docstring
-        # TODO: Consider making this more standardized.
-        #  Currently fast_log_loss needs a bit of special preprocessing of the data and isn't a straightforward replace.
-        """
-        if metric_name == 'log_loss':
-            # TODO: Can be even faster if we transform pred_val and pred_test
-            #  as a preprocessing step to TabularModelPredictions.
-            #  This would avoid ever having to pay the preprocessing time cost, and would massively reduce memory usage.
-            eval_metric = _fast_log_loss.fast_log_loss
-        elif metric_name == 'roc_auc':
-            eval_metric = _fast_roc_auc.fast_roc_auc_cpp
-        else:
-            eval_metric = get_metric(metric=metric_name, problem_type=problem_type)
-        return eval_metric
-
     def run_task(self, task: str, models: List[str]) -> Tuple[float, np.array]:
         fold = self.dataset_name_to_fold_dict[task]
         tid = self.dataset_name_to_tid_dict[task]
-
-        problem_type = self.zeroshot_gt[tid][fold]['problem_type']
-        metric_name = self.zeroshot_gt[tid][fold]['eval_metric']
-        y_val = self.zeroshot_gt[tid][fold]['y_val']
-        y_test = self.zeroshot_gt[tid][fold]['y_test']
-
-        y_val = y_val.to_numpy()
-        y_test = y_test.fillna(-1).to_numpy()
-
-        pred_val, pred_test = self.zeroshot_pred_proba.predict(dataset=tid, fold=fold, splits=['val', 'test'], models=models)
-
-        if problem_type == 'binary':
-            # Force binary prediction probabilities to 1 dimensional prediction probabilites of the positive class
-            # if it is in multiclass format
-            if len(pred_val.shape) == 3:
-                pred_val = pred_val[:, :, 1]
-            if len(pred_test.shape) == 3:
-                pred_test = pred_test[:, :, 1]
-
-        fit_metric_name = self.proxy_fit_metric_map.get(metric_name, metric_name)
-
-        eval_metric = self._get_metric_from_name(metric_name=metric_name, problem_type=problem_type)
-        fit_eval_metric = self._get_metric_from_name(metric_name=fit_metric_name, problem_type=problem_type)
-
-        if hasattr(fit_eval_metric, 'preprocess_bulk'):
-            y_val, pred_val = fit_eval_metric.preprocess_bulk(y_val, pred_val)
-
-        weighted_ensemble = EnsembleSelection(
-            ensemble_size=self.ensemble_size,
-            problem_type=problem_type,
-            metric=fit_eval_metric,
-            **self.ensemble_selection_kwargs,
-        )
-
-        weighted_ensemble.fit(predictions=pred_val, labels=y_val)
-
-        if hasattr(eval_metric, 'preprocess_bulk'):
-            y_test, pred_test = eval_metric.preprocess_bulk(y_test, pred_test)
-
-        if eval_metric.needs_pred:
-            y_test_pred = weighted_ensemble.predict(pred_test)
-        else:
-            y_test_pred = weighted_ensemble.predict_proba(pred_test)
-        err = eval_metric.error(y_test, y_test_pred)
-
-        # FIXME
-        # y_val_pred = weighed_ensemble.predict_proba(a)
-        # errval = eval_metric._optimum - eval_metric(y_val, y_val_pred)  # FIXME: proba or pred, figure out
-        # print(dataset, errval)
-
-        ensemble_weights: np.array = weighted_ensemble.weights_
-
-        return err, ensemble_weights
+        return self.ensemble_scorer.run_task(dataset=tid, fold=fold, models=models)
 
     def compute_errors(self, configs: List[str]) -> Tuple[Dict[str, float], Dict[str, np.array]]:
         """
