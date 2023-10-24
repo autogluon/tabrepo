766125ebf5bf6c41b0480b82b6afbc22f51a2873
innixma <innixma@gmail.com>
2023-10-24 20:50:32 +0000

    update default repo

--- scripts/baseline_comparison/evaluate_baselines.py
+++ scripts/baseline_comparison/evaluate_baselines.py
@@ -208,7 +208,7 @@ def save_total_runtime_to_file(total_time_h):
 if __name__ == "__main__":
     from argparse import ArgumentParser
     parser = ArgumentParser()
-    parser.add_argument("--repo", type=str, help="Name of the repo to load", default="BAG_D244_F3_C1416")
+    parser.add_argument("--repo", type=str, help="Name of the repo to load", default="D244_F3_C1416_200")
     parser.add_argument("--n_folds", type=int, default=-1, required=False,
                         help="Number of folds to consider when evaluating all baselines. Uses all if set to -1.")
     parser.add_argument("--n_datasets", type=int, required=False, help="Number of datasets to consider when evaluating all baselines.")
