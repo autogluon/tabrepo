2d35db0cee17ff00c26f0e226eeb42a1013a3604
David Salinas <david.salinas.pro@gmail.com>
2023-10-27 14:50:46 +0200

    update readme

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = [
 setup(
     name='tabrepo',
     version='0.0.1',
-    packages=find_packages(exclude=('docs', 'tst', 'scripts', 'data')),
+    packages=find_packages(exclude=('docs', 'tst', 'data')),
     url='https://github.com/Innixma/autogluon-zeroshot-private',
     license='Apache-2.0',
     author='AutoGluon Community',
