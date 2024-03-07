77cf5f0914b001d0559885914ad5581d128c8113
David Salinas <david.salinas.pro@gmail.com>
2024-03-07 09:33:41 +0100

    Download files in parallel

--- tabrepo/utils/download.py
+++ tabrepo/utils/download.py
@@ -4,15 +4,19 @@ import os
 import urllib.request
 from tqdm import tqdm
 
+from tabrepo.utils.parallel_for import parallel_for
+
 
 def download_files(remote_to_local_tuple_list: list, dry_run: bool = False, verbose: bool = False):
-    num_files = len(remote_to_local_tuple_list)
-    for i in tqdm(range(num_files), disable=(not verbose) or dry_run, desc="Downloading Files"):
-        remote_path, local_path = remote_to_local_tuple_list[i]
+    def download_file(remote_path, local_path, dry_run):
         if dry_run:
             print(f'Dry Run: Would download file "{remote_path}" to "{local_path}"')
-            continue
+            return
         directory = os.path.dirname(local_path)
         if directory not in ["", "."]:
             pathlib.Path(directory).mkdir(parents=True, exist_ok=True)
         urllib.request.urlretrieve(remote_path, local_path)
+
+    if verbose:
+        remote_to_local_tuple_list = tqdm(remote_to_local_tuple_list, desc="Downloading Files")
+    parallel_for(download_file, inputs=remote_to_local_tuple_list, context={"dry_run": dry_run})
