02264e8316e7b62a4f01dde4840e802a8e940bdd
innixma <innixma@gmail.com>
2025-01-10 23:55:28 +0000

    Add docstrings, code cleanup

--- tabrepo/repository/abstract_repository.py
+++ tabrepo/repository/abstract_repository.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import copy
 from abc import ABC, abstractmethod
+from pathlib import Path
 from typing import Dict, List
 
 import numpy as np
@@ -563,6 +564,7 @@ class AbstractRepository(ABC, SaveLoadMixin):
     # TODO: repo time_infer_s is per row, results_df is the total time for all rows, need to align later
     # TODO: Error if unknown configs/baselines requested
     # TODO: Add fillna
+    # TODO: Docstring
     # Q:Whether to keep these functions a part of TabRepo or keep them separate as a part of new fit()-package
     def compare_metrics(
         self,
@@ -616,21 +618,55 @@ class AbstractRepository(ABC, SaveLoadMixin):
 
         return df
 
-    def plot_overall_rank_comparison(self, results_df: pd.DataFrame, save_dir: str, evaluator_kwargs: dict = None, calibration_framework: str = None):
-        from autogluon_benchmark.evaluation.evaluator import Evaluator
-        from autogluon_benchmark.plotting.plotter import Plotter
+    # TODO: Rename to something better?
+    def plot_overall_rank_comparison(
+        self,
+        results_df: pd.DataFrame,
+        save_dir: str,
+        evaluator_kwargs: dict = None,
+        calibration_framework: str = None,
+    ) -> "EvaluatorOutput":
+        """
+        Requires `autogluon_benchmark` to be installed.
+
+        Parameters
+        ----------
+        results_df: pd.DataFrame
+            The input data to calculate metrics with.
+            An easy way to obtain a valid `results_df` is to call `repo.compare_metrics(...)`
+            It should have a multi-index of (dataset, fold, framework), with the following columns:
+                metric_error: float
+                metric: str
+                time_train_s: float
+                time_infer_s: float
+                problem_type: str
+        save_dir: str
+            The local directory to save comparison results and figures to.
+        evaluator_kwargs: dict, default = None
+            The evaluator kwargs.
+        calibration_framework: str, default = None
+            The framework to fix at 1000 elo.
+
+        Returns
+        -------
+        EvaluatorOutput object from autogluon_benchmark
+        """
+        try:
+            from autogluon_benchmark.evaluation.evaluator import Evaluator
+            from autogluon_benchmark.plotting.plotter import Plotter
+        except ImportError:
+            raise ImportError(f"To use `repo.plot_overall_rank_comparison, you must first install autogluon_benchmark.")
         if evaluator_kwargs is None:
             evaluator_kwargs = {}
         results_df = results_df.reset_index().copy()
         results_df["tid"] = results_df["dataset"].apply(self.dataset_to_tid)
         evaluator = Evaluator(task_metadata=self.task_metadata, **evaluator_kwargs)
         evaluator_output = evaluator.transform(results_df)
-        output_path = f"{save_dir}"
-        # FIXME: Use Path, don't use /
-        figure_savedir = f"{output_path}/figures"
-        save_pd.save(path=f"{output_path}/results.csv", df=results_df)
-        save_pd.save(path=f"{output_path}/results_ranked_agg.csv", df=evaluator_output.results_ranked_agg)
-        save_pd.save(path=f"{output_path}/results_ranked.csv", df=evaluator_output.results_ranked)
+        output_path = Path(save_dir)
+        figure_savedir = str(output_path / "figures")
+        save_pd.save(path=str(output_path / "results.csv"), df=results_df)
+        save_pd.save(path=str(output_path / "results_ranked_agg.csv"), df=evaluator_output.results_ranked_agg)
+        save_pd.save(path=str(output_path / "results_ranked.csv"), df=evaluator_output.results_ranked)
 
         plotter = Plotter(
             results_ranked_fillna_df=evaluator_output.results_ranked,
