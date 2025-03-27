aa88da4cf66d2d632eee6d3c8455ecab2c438241
Prateek M Desai <prateekmdesai04@gmail.com>
2025-03-27 21:56:26 +0000

    clean docker build

--- scripts/tabflow/docker/build_docker.sh
+++ scripts/tabflow/docker/build_docker.sh
@@ -12,12 +12,27 @@ SOURCE_ACCOUNT=$3
 TARGET_ACCOUNT=$4
 REGION=$5
 
+# Check if all required directories exist, this is specific for TabRepo
+# For future integrations, this should be updated with the required directories for Docker build
+REQUIRED_DIRS=("autogluon" "autogluon-benchmark" "tabrepo")
+BASE_DIR="$(cd ../../../.. && pwd)"
+
+for dir in "${REQUIRED_DIRS[@]}"; do
+    if [ ! -d "${BASE_DIR}/${dir}" ]; then
+        echo "Error: Required directory '${dir}' does not exist in ${BASE_DIR}"
+        exit 1
+    fi
+done
+
+echo "All required directories exist. Proceeding with Docker build."
+cp ./.dockerignore ${BASE_DIR}/.dockerignore
+
 # Login to AWS ECR
 aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${SOURCE_ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com
 aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${TARGET_ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com
 
 # Build the Docker image
-docker build --no-cache -t ${REPO_NAME} -f ./Dockerfile_SM ../..
+docker build --no-cache -t ${REPO_NAME} -f ./Dockerfile_SM ${BASE_DIR}
 
 # Tag the Docker image
 docker tag ${REPO_NAME}:latest ${TARGET_ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com/${REPO_NAME}:${TAG}
