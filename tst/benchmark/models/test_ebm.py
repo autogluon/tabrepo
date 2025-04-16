2ae1da1bc096f164af5fb75b85e597923e350aef
Nick Erickson <innixma@gmail.com>
2025-04-16 13:05:52 -0700

    Add unit tests for new models (#131)
    * Add unit tests for new models
    
    * fix imports

---
+++ tst/benchmark/models/test_ebm.py
@@ -0,0 +1,15 @@
+
+def test_ebm():
+    model_hyperparameters = {}
+
+    try:
+        from autogluon.tabular.testing import FitHelper
+        from tabrepo.benchmark.models.ag.ebm.ebm_model import ExplainableBoostingMachineModel
+        model_cls = ExplainableBoostingMachineModel
+        FitHelper.verify_model(model_cls=model_cls, model_hyperparameters=model_hyperparameters)
+    except ImportError as err:
+        print(
+            f"Import Error, skipping test... "
+            f"Ensure you have the proper dependencies installed to run this test:\n"
+            f"{err}"
+        )
---
+++ tst/benchmark/models/test_tabicl.py
@@ -0,0 +1,16 @@
+
+
+def test_tabicl():
+    model_hyperparameters = {"n_estimators": 1}
+
+    try:
+        from autogluon.tabular.testing import FitHelper
+        from tabrepo.benchmark.models.ag.tabicl.tabicl_model import TabICLModel
+        model_cls = TabICLModel
+        FitHelper.verify_model(model_cls=model_cls, model_hyperparameters=model_hyperparameters)
+    except ImportError as err:
+        print(
+            f"Import Error, skipping test... "
+            f"Ensure you have the proper dependencies installed to run this test:\n"
+            f"{err}"
+        )
