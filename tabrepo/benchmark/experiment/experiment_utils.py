6573ca401899815f745d0a3030c566bdba6fe587
innixma <innixma@gmail.com>
2025-04-21 23:16:43 +0000

    remove old code

--- tabrepo/benchmark/experiment/experiment_utils.py
+++ tabrepo/benchmark/experiment/experiment_utils.py
@@ -214,60 +214,6 @@ class ExperimentBatchRunner:
 
         return repo
 
-    # TODO: Maybe calibrating model binary pred proba will improve ensemble roc_auc?
-    def temp_scale(self, y_val, y_pred_proba_val, method: str = "v2"):
-        init_val = 1.0
-        max_iter = 200
-        lr = 0.1
-        from tabrepo.utils.temp_scaling.calibrators import AutoGluonTemperatureScalingCalibrator, TemperatureScalingCalibrator, AutoGluonTemperatureScalingCalibratorFixed, TemperatureScalingCalibratorFixed
-        if method == "v1":
-            calibrator = AutoGluonTemperatureScalingCalibrator(init_val=init_val, max_iter=max_iter, lr=lr)
-        elif method == "v2":
-            calibrator = TemperatureScalingCalibrator(max_iter=max_iter, lr=lr)
-        elif method == "v1_fix":
-            calibrator = AutoGluonTemperatureScalingCalibratorFixed(init_val=init_val, max_iter=max_iter, lr=lr)
-        elif method == "v2_fix":
-            calibrator = TemperatureScalingCalibratorFixed(max_iter=max_iter, lr=lr)
-        else:
-            raise ValueError(f"Unknown temp_scale method: {method}")
-        calibrator.fit(X=y_pred_proba_val, y=y_val)
-        return calibrator
-
-    def generate_calibrated(self, result, method: str = "v2", name_suffix: str = "_CAL"):
-        sim_artifact = result["simulation_artifacts"]
-        metric = sim_artifact["metric"]
-        from autogluon.core.metrics import get_metric
-        problem_type = sim_artifact["problem_type_transform"]
-        ag_metric = get_metric(metric=metric, problem_type=problem_type)
-        y_test = sim_artifact["y_test"]
-
-        y_val = sim_artifact["y_val"]
-        y_pred_proba_val = sim_artifact["pred_val"]
-        calibrator = self.temp_scale(y_val=y_val, y_pred_proba_val=y_pred_proba_val, method=method)
-        y_pred_proba_test = sim_artifact["pred_test"]
-        y_pred_proba_test_scaled = calibrator.predict_proba(y_pred_proba_test)
-        y_pred_proba_val_scaled = calibrator.predict_proba(y_pred_proba_val)
-
-        # metric_error_og = ag_metric.error(y_test, y_pred_proba_test)
-        metric_error_cal = ag_metric.error(y_test, y_pred_proba_test_scaled)
-        metric_error_val_og = ag_metric.error(y_val, y_pred_proba_val)
-        metric_error_val_cal = ag_metric.error(y_val, y_pred_proba_val_scaled)
-
-        if metric_error_val_cal > metric_error_val_og:
-            print(f"WARNING:")
-            print(metric_error_val_cal, metric_error_val_og)
-            print(result["framework"], result["dataset"], result["fold"])
-
-        result_calibrated = copy.deepcopy(result)
-        result_calibrated["metric_error"] = metric_error_cal
-        result_calibrated["metric_error_val"] = metric_error_val_cal
-        result_calibrated["simulation_artifacts"]["pred_test"] = y_pred_proba_test_scaled
-        result_calibrated["simulation_artifacts"]["pred_val"] = y_pred_proba_val_scaled
-        result_calibrated["framework"] = result_calibrated["framework"] + name_suffix
-        # FIXME: Fix bag children? Should they be calibrated?
-
-        return result_calibrated
-
     def _align_result_input_format(self, result: dict | BaselineResult) -> BaselineResult:
         """
         Converts results in old format to new format
