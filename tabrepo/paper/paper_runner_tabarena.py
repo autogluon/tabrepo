aa3eea08466030d8d265600620b8d327f7a5155c
innixma <innixma@gmail.com>
2025-05-09 21:49:50 +0000

    update paper_runner_tabarena.py

--- tabrepo/paper/paper_runner_tabarena.py
+++ tabrepo/paper/paper_runner_tabarena.py
@@ -1,16 +1,11 @@
 from __future__ import annotations
 
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from tabrepo import EvaluationRepository, Evaluator
-from scripts.baseline_comparison.plot_utils import (
-    figure_path,
-)
 from scripts.baseline_comparison.evaluate_utils import plot_family_proportion
-from tabrepo.paper.paper_utils import make_scorers, generate_sensitivity_plots, get_framework_type_method_names
+from tabrepo.paper.paper_utils import make_scorers, generate_sensitivity_plots
 from scripts.dataset_analysis import generate_dataset_analysis
 from .paper_runner import PaperRun
+from tabrepo.tabarena.tabarena import TabArena
 
 
 class PaperRunTabArena(PaperRun):
@@ -62,6 +57,13 @@ class PaperRunTabArena(PaperRun):
         return df_results_all
 
     def eval(self, df_results: pd.DataFrame, use_gmean: bool = False):
+        # FIXME: Clean bad columns, in future ensure these don't exist
+        df_results = df_results.drop(columns=[
+            "normalized_error",
+            "index",
+        ])
+
+        # df_results = self.evaluator.compare_metrics(results_df=df_results, configs=[], baselines=[], keep_extra_columns=True, fillna=True)
         framework_types = [
             "GBM",
             "XGB",
@@ -72,7 +74,8 @@ class PaperRunTabArena(PaperRun):
             "RF",
             "XT",
             "LR",
-            "TABPFN",
+            "TABPFNV2",
+            "TABICL",
             "REALMLP",
             "EBM",
             "FT_TRANSFORMER",
@@ -89,9 +92,9 @@ class PaperRunTabArena(PaperRun):
             "framework": "method",
         })
         df_results["method"] = df_results["method"].map({
-            "AutoGluon_bq_4h8c_2023_11_14": "AutoGluon 0.8 (4h)",
-            "AutoGluon_bq_4h8c_2024_10_25": "AutoGluon 1.1 (4h)",
-            "autosklearn2_4h8c_2023_11_14": "Autosklearn2 (4h)",
+            "AutoGluon_bq_4h8c": "AutoGluon 1.3 (4h)",
+            "AutoGluon_bq_1h8c": "AutoGluon 1.3 (1h)",
+            "AutoGluon_bq_5m8c": "AutoGluon 1.3 (5m)",
             "LightGBM_c1_BAG_L1": "GBM (default)",
             "XGBoost_c1_BAG_L1": "XGB (default)",
             "CatBoost_c1_BAG_L1": "CAT (default)",
@@ -105,29 +108,127 @@ class PaperRunTabArena(PaperRun):
             "RealMLP_c1_BAG_L1": "REALMLP (default)",
             "ExplainableBM_c1_BAG_L1": "EBM (default)",
             "FTTransformer_c1_BAG_L1": "FT_TRANSFORMER (default)",
+            "TabPFNv2_c1_BAG_L1": "TABPFNV2 (default)",
+            "TabICL_c1_BAG_L1": "TABICL (default)",
         }).fillna(df_results["method"])
         print(df_results)
         rank_scorer, normalized_scorer = make_scorers(self.repo)
-        df_results["normalized-error"] = [normalized_scorer.rank(task=(dataset, fold), error=error) for (dataset, fold, error) in zip(df_results["dataset"], df_results["fold"], df_results["metric_error"])]
+        df_results["normalized-error-task"] = [normalized_scorer.rank(task=(dataset, fold), error=error) for (dataset, fold, error) in zip(df_results["dataset"], df_results["fold"], df_results["metric_error"])]
         df_results["seed"] = 0
+
+        df_results_baselines = pd.concat([
+            self.repo._zeroshot_context.df_configs_ranked,
+            self.repo._zeroshot_context.df_baselines,
+        ], ignore_index=True)
+
+        df_comparison_per_dataset = df_results_baselines.groupby(["framework", "dataset"])["metric_error"].mean()
+        df_comparison_per_dataset = df_comparison_per_dataset.reset_index(drop=False)
+        df_comparison_per_dataset = df_comparison_per_dataset.rename(columns={
+            "framework": "method",
+        })
+        df_results_per_dataset = df_results.groupby(["method", "dataset"])["metric_error"].mean().reset_index(drop=False)
+        from tabrepo.utils.normalized_scorer import NormalizedScorer
+
+        # Standard normalized-error, only computed off of real experiments, not impacted by simulation runs.
+        # This is biased against very strong simulation results because they can't get better than `0.0` on a dataset.
+        normalized_scorer_dataset = NormalizedScorer(df_comparison_per_dataset, tasks=list(df_results_baselines["dataset"].unique()), baseline=None, task_col="dataset", framework_col="method")
+
+        # Alternative, this also incorporates Portfolios and HPO into the normalized scoring. This makes normalized-error dependent on what simulations we run.
+        # This is unbiased against very strong simulation results because the best method defines what is `0.0` on a dataset.
+        # normalized_scorer_dataset = NormalizedScorer(df_results_per_dataset, tasks=list(df_results_per_dataset["dataset"].unique()), baseline=None,
+        #                                              task_col="dataset", framework_col="method")
+
+        df_results_per_dataset["normalized-error-dataset"] = [normalized_scorer_dataset.rank(task=dataset, error=error) for (dataset, error) in
+                                          zip(df_results_per_dataset["dataset"], df_results_per_dataset["metric_error"])]
+
+        df_results_per_dataset = df_results_per_dataset.set_index(["dataset", "method"], drop=True)["normalized-error-dataset"]
+        df_results = df_results.merge(df_results_per_dataset, left_on=["dataset", "method"], right_index=True)
+        df_results["normalized-error"] = df_results["normalized-error-dataset"]
 
         import copy
         df_results_rank_compare = copy.deepcopy(df_results)
         df_results_rank_compare = df_results_rank_compare.rename(columns={"method": "framework"})
 
-        self.plot_tuning_impact(df=df_results, framework_types=framework_types, save_prefix="tmp/v2_mini", use_gmean=use_gmean)
+        baselines = [
+            "AutoGluon 1.3 (5m)",
+            # "AutoGluon 1.3 (1h)",
+            "AutoGluon 1.3 (4h)",
+            "Portfolio-fix_fillna-N50 (ensemble) (4h)",
+        ]
+        baseline_colors = [
+            "darkgray",
+            "black",
+            "blue",
+            # "red",
+        ]
 
-        # df_results_realmlp_alt = df_results[df_results["method"].str.contains("RealMLP_r") & df_results["method"].str.contains("_alt_")]
-        # df_results_realmlp_og = df_results[df_results["method"].str.contains("RealMLP_r") & ~df_results["method"].str.contains("_alt_")]
-        #
-        # df_results_only_og = df_results.drop(index=df_results_realmlp_alt.index)
-        # df_results_only_alt = df_results.drop(index=df_results_realmlp_og.index)
-        # self.plot_tuning_impact(df=df_results_only_alt, framework_types=framework_types, save_prefix="tmp/v2_mini_alt", use_gmean=use_gmean)
-        #
-        # self.plot_tuning_impact(df=df_results_only_og, framework_types=framework_types, save_prefix="tmp/v2_mini_main", use_gmean=use_gmean)
+        self.plot_tuning_impact(
+            df=df_results,
+            framework_types=framework_types,
+            save_prefix=f"{self.output_dir}",
+            use_gmean=use_gmean,
+            baselines=baselines,
+            baseline_colors=baseline_colors,
+        )
 
         df_results_rank_compare2 = df_results_rank_compare[~df_results_rank_compare["framework"].str.contains("_BAG_L1") & ~df_results_rank_compare["framework"].str.contains("_r")]
-        self.evaluator.plot_overall_rank_comparison(results_df=df_results_rank_compare2, save_dir="tmp/paper_v2")
+
+        tabarena = TabArena(
+            method_col="framework",
+            task_col="dataset",
+            seed_column="fold",
+            error_col="metric_error",
+            columns_to_agg_extra=[
+                "time_train_s",
+                "time_infer_s",
+                "normalized-error",
+                "normalized-error-task",
+            ],
+            groupby_columns=[
+                "metric",
+                "problem_type",
+            ],
+        )
+
+        leaderboard = tabarena.leaderboard(
+            data=df_results_rank_compare2,
+            include_mrr=True,
+            # include_failure_counts=True,
+            include_rank_counts=True,
+            include_elo=True,
+        )
+
+        from autogluon.common.savers import save_pd
+        save_pd.save(path=f"{self.output_dir}/tabarena_leaderboard.csv", df=leaderboard)
+
+        with pd.option_context("display.max_rows", None, "display.max_columns", None, "display.width", 1000):
+            print(leaderboard)
+
+        results_per_task = tabarena.compute_results_per_task(data=df_results_rank_compare2)
+        tabarena.plot_critical_diagrams(results_per_task=results_per_task, save_path=f"{self.output_dir}/figures/critical-diagram.png")
+        tabarena.plot_critical_diagrams(results_per_task=results_per_task, save_path=f"{self.output_dir}/figures/critical-diagram.pdf")
+
+        results_per_task_rename = results_per_task.rename(columns={
+            tabarena.method_col: "framework",
+            tabarena.task_col: "dataset",
+            tabarena.error_col: "metric_error",
+        })
+
+        from autogluon_benchmark.plotting.plotter import Plotter
+        plotter = Plotter(
+            results_ranked_df=results_per_task_rename,
+            results_ranked_fillna_df=results_per_task_rename,
+            save_dir=f"{self.output_dir}/figures/plotter"
+        )
+
+        calibration_framework = "RF (default)"
+
+        plotter.plot_all(
+            calibration_framework=calibration_framework,
+            calibration_elo=1000,
+        )
+
+        self.evaluator.plot_overall_rank_comparison(results_df=df_results_rank_compare2, save_dir=f"{self.output_dir}/paper_v2")
 
         hue_order_family_proportion = [
             "RealMLP",
@@ -139,13 +240,13 @@ class PaperRunTabArena(PaperRun):
             "ExtraTrees",
             "LinearModel",
             "KNeighbors",
-            # "TabPFNv2",
-            "TabPFN",
+            "TabPFNv2",
+            "TabICL",
             # "TabForestPFN",
             "ExplainableBM",
             "NeuralNetFastAI",
             "FTTransformer",
         ]
 
-        plot_family_proportion(df=df_results, save_prefix="tmp/family_prop", method="Portfolio-N200 (ensemble) (4h)", hue_order=hue_order_family_proportion)
-        plot_family_proportion(df=df_results, save_prefix="tmp/family_prop2", method="Portfolio-fix_fillna-N200 (ensemble) (4h)", hue_order=hue_order_family_proportion)
+        plot_family_proportion(df=df_results, save_prefix=f"{self.output_dir}/family_prop", method="Portfolio-N200 (ensemble) (4h)", hue_order=hue_order_family_proportion)
+        plot_family_proportion(df=df_results, save_prefix=f"{self.output_dir}/family_prop2", method="Portfolio-fix_fillna-N200 (ensemble) (4h)", hue_order=hue_order_family_proportion)
