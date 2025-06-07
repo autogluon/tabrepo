fix training metric calculation

tabrepo/benchmark/models/ag/mitra/_internal/core/prediction_metrics.py
@@ -111,17 +111,19 @@ class PredictionMetricsTracker():
         self.ys_true: list[np.ndarray] = []
 
 
-    def update(self, y_pred: torch.Tensor, y_true: torch.Tensor) -> None:
+    def update(self, y_pred: torch.Tensor, y_true: torch.Tensor, train: bool) -> None:
 
         y_pred_np = y_pred.detach().cpu().numpy()[0]
         y_pred_ori = self.preprocessor.inverse_transform_y(y_pred_np)
 
         y_true_np = y_true.detach().cpu().numpy()[0]
-        # y_true_ori = self.preprocessor.inverse_transform_y(y_true_np)
+        if train:
+            y_true_ori = self.preprocessor.inverse_transform_y(y_true_np)
+        else:
+            y_true_ori = y_true_np
         # y_true_ori = np.zeros((len(y_true_np), len(np.unique(y_true_np))))
         # for i in range(len(y_true_ori)):
         #     y_true_ori[i, int(y_true_np[i])] = 1
-        y_true_ori = y_true_np
 
         self.ys_pred.append(y_pred_ori)
         self.ys_true.append(y_true_ori)

tabrepo/benchmark/models/ag/mitra/_internal/core/trainer_finetune.py
@@ -147,7 +147,7 @@ class TrainerFinetune(BaseEstimator):
                     y_hat = self.bins[y_hat] + self.bin_width / 2
 
                 y_hat = y_hat.float()
-                prediction_metrics_tracker.update(y_hat, y_query)
+                prediction_metrics_tracker.update(y_hat, y_query, train=True)
 
             metrics_train = prediction_metrics_tracker.get_metrics()
             metrics_valid = self.evaluate(x_train, y_train, x_val, y_val)   
@@ -225,7 +225,7 @@ class TrainerFinetune(BaseEstimator):
                     y_hat = self.bins[y_hat] + self.bin_width / 2
 
                 y_hat = y_hat.float()
-                prediction_metrics_tracker.update(y_hat, y_q)
+                prediction_metrics_tracker.update(y_hat, y_q, train=False)
 
         metrics_eval = prediction_metrics_tracker.get_metrics()
         return metrics_eval
@@ -303,6 +303,22 @@ class TrainerFinetune(BaseEstimator):
                 pad_to_max_features = False
             case _:
                 raise NotImplementedError(f"Model {self.cfg.model_name} not implemented")
+        
+        g = torch.Generator()
+        g.manual_seed(0)
+
+        def set_seed(seed: int) -> None:
+            import random
+            random.seed(seed)
+            np.random.seed(seed)
+            torch.manual_seed(seed)
+            torch.cuda.manual_seed(seed)
+            torch.cuda.manual_seed_all(seed)
+
+        def seed_worker(worker_id: int) -> None:
+            worker_seed = torch.initial_seed() % 2**32
+            set_seed(worker_seed)
+
 
         return torch.utils.data.DataLoader(
             dataset,
@@ -315,6 +331,9 @@ class TrainerFinetune(BaseEstimator):
                 max_features=self.cfg.hyperparams['dim_embedding'],
                 pad_to_max_features=pad_to_max_features
             ),
+            worker_init_fn=seed_worker,
+            generator=g,
+
         )
     
 

tabrepo/benchmark/models/ag/mitra/_internal/data/dataset_split.py
@@ -3,22 +3,22 @@ from sklearn.model_selection import StratifiedKFold, train_test_split
 
 from ..._internal.config.enums import Task
 
-def make_dataset_split(x: np.ndarray, y: np.ndarray, task: Task) -> tuple[np.ndarray, ...]:
+def make_dataset_split(x: np.ndarray, y: np.ndarray, task: Task, seed: int) -> tuple[np.ndarray, ...]:
     # Splits the dataset into train and validation sets with ratio 80/20
 
     if task == Task.REGRESSION:
-        return make_standard_dataset_split(x, y)
+        return make_standard_dataset_split(x, y, seed=seed)
 
     size_of_smallest_class = np.min(np.bincount(y))
 
     if size_of_smallest_class >= 5:
         # stratification needs have at least 5 samples in each class if split is 80/20
-        return make_stratified_dataset_split(x, y)
+        return make_stratified_dataset_split(x, y, seed=seed)
     else:
-        return make_standard_dataset_split(x, y)
+        return make_standard_dataset_split(x, y, seed=seed)
     
 
-def make_stratified_dataset_split(x, y, n_splits=5):
+def make_stratified_dataset_split(x, y, n_splits=5, seed=42):
 
     # Stratify doesn't shuffle the data, so we shuffle it first
     permutation = np.random.permutation(len(y))
@@ -36,9 +36,9 @@ def make_stratified_dataset_split(x, y, n_splits=5):
         n_splits = max(2, n_splits)
     else:
         # If we can't do stratified splitting, fall back to standard split
-        return make_standard_dataset_split(x, y)
+        return make_standard_dataset_split(x, y, seed)
 
-    skf = StratifiedKFold(n_splits=n_splits, shuffle=True)
+    skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=seed)
     indices = next(skf.split(x, y))
     x_t_train, x_t_valid = x[indices[0]], x[indices[1]] # 80%, 20%
     y_t_train, y_t_valid = y[indices[0]], y[indices[1]]
@@ -46,8 +46,8 @@ def make_stratified_dataset_split(x, y, n_splits=5):
     return x_t_train, x_t_valid, y_t_train, y_t_valid
 
 
-def make_standard_dataset_split(x, y):
+def make_standard_dataset_split(x, y, seed):
         
     return train_test_split(
-        x, y, test_size=0.2
+        x, y, test_size=0.2, random_state=seed,
     )

tabrepo/benchmark/models/ag/mitra/sklearn_interface.py
@@ -99,6 +99,7 @@ class MitraBase(BaseEstimator):
         self.trainers = []
         self.models = []
         self.train_time = 0
+        self.seed = 0
 
 
     def _create_config(self, task, dim_output):
@@ -156,7 +157,7 @@ class MitraBase(BaseEstimator):
     def _split_data(self, X, y):
         """Split data into training and validation sets."""
         if hasattr(self, 'task') and self.task == 'classification':
-            return make_stratified_dataset_split(X, y)
+            return make_stratified_dataset_split(X, y, seed=self.seed)
         else:
             # For regression, use random split
             val_indices = np.random.choice(range(len(X)), int(DEFAULT_VALIDATION_SPLIT * len(X)), replace=False).tolist()

tst/benchmark/models/test_mitra.py
@@ -13,6 +13,7 @@ import time
 import os
 import pandas as pd
 from sklearn.metrics import roc_auc_score, log_loss, accuracy_score, root_mean_squared_error, r2_score, mean_squared_error, mean_absolute_error
+import random
 
 def test_mitra():
     model_hyperparameters = {"n_estimators": 1}
@@ -128,6 +129,13 @@ def run_bagging(task_id, fold, bagging=True, target_dataset="tabrepo10fold", fil
 
 if __name__ == "__main__":
 
+    seed = 0
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
     # test_mitra()
 
     # 8 * 6 + 9 * 2 = 66
@@ -160,6 +168,8 @@ if __name__ == "__main__":
                 360945, 361235, 361236, 361237, 361617, 361243, 361619, 361621, 361251, 361256, \
                 361258, 361259, 361622, 359934, 359933, 359950, 359932, 359931, 359930]
 
+    test_reg = [363612]
+
     # dataset_name, target_dataset, start, end = tabzilla, "tabzilla10fold", 65, 75
 
     # for did in dataset_name[start:end]:
@@ -168,10 +178,10 @@ if __name__ == "__main__":
 
     #         run_bagging(task_id=did, fold=fold, bagging=True, target_dataset=target_dataset, file_name=f"mitra_bagging_ft_{start}_{end}", t="classification")  
 
-    dataset_name, target_dataset, start, end = nature_reg, "nature_regression10fold", 0, 28
+    dataset_name, target_dataset, start, end = test_reg, "tabrepo10fold", 0, 1
 
     for did in dataset_name[start:end]:
 
-        for fold in range(10):
+        for fold in range(1):
 
             run_bagging(task_id=did, fold=fold, bagging=True, target_dataset=target_dataset, file_name=f"mitra_bagging_ft_{start}_{end}", t="regression")  