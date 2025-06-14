b69f050d06fa03a059226df439e5fb1722dec6ce
Paul Koch <46825734+paulbkoch@users.noreply.github.com>
2025-06-14 09:25:35 -0700

    change min_cat_samples and cat_smooth to ranges and add more interactions

--- tabrepo/models/ebm/generate.py
+++ tabrepo/models/ebm/generate.py
@@ -10,13 +10,13 @@ search_space = {
     'max_leaves': Int(2, 3, default=2),
     'smoothing_rounds': Int(0, 1000, default=200),
     'learning_rate': Real(0.0025, 0.2, default=0.02, log=True),
-    'interactions': Categorical(0, "1x", "2x", "3x", "4x", "5x", "6x", "7x", "8x", "9x"),
+    'interactions': Categorical(0, "0.5x", "1x", "1.5x", "2x", "2.5x", "3x", "3.5x", "4x", "4.5x", "5x", "6x", "7x", "8x", "9x", "10x", "15x", "20x", "25x"),
     'interaction_smoothing_rounds': Int(0, 200, default=90),
     'min_hessian': Real(1e-10, 1e-2, default=1e-4, log=True),
     'min_samples_leaf': Int(2, 20, default=4),
     'gain_scale': Real(0.5, 5.0, default=5.0, log=True),
-    'min_cat_samples': Categorical(5, 10, 15, 20),
-    'cat_smooth': Categorical(5.0, 10.0, 20.0, 100.0),
+    'min_cat_samples': Int(5, 20, default=10),
+    'cat_smooth': Real(5.0, 100.0, default=10.0, log=True),
     'missing': Categorical('separate', 'low', 'high', 'gain'),
 }
 
