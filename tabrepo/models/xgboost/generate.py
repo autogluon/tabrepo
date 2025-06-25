b14e2a5da27940d78110d2d13ae0177a9b73320c
LennartPurucker <contact@lennart-purucker.com>
2025-06-25 12:22:03 +0200

    make optimized search spaces the default and remove old search spaces

--- tabrepo/models/xgboost/generate.py
+++ tabrepo/models/xgboost/generate.py
@@ -1,27 +1,44 @@
-from autogluon.common.space import Real, Int, Categorical
+from __future__ import annotations
+
 from autogluon.tabular.models import XGBoostModel
+from ConfigSpace import Categorical, ConfigurationSpace, Float, Integer
 
-from ...utils.config_utils import ConfigGenerator
-
-
-name = 'XGBoost'
-manual_configs = [
-    {},
-    {'learning_rate': 1e-2},
-    {'enable_categorical': True},
-    {'learning_rate': 1e-2, 'enable_categorical': True},
-]
-search_space = {
-    'learning_rate': Real(lower=5e-3, upper=0.1, default=0.1, log=True),
-    'max_depth': Int(lower=4, upper=10, default=6),
-    'min_child_weight': Real(0.5, 1.5, default=1.0),
-    'colsample_bytree': Real(0.5, 1.0, default=1.0),
-    'enable_categorical': Categorical(True, False),
-}
-
-gen_xgboost = ConfigGenerator(model_cls=XGBoostModel, manual_configs=[{}], search_space=search_space)
+from tabrepo.models.utils import convert_numpy_dtypes
+from tabrepo.utils.config_utils import CustomAGConfigGenerator
 
 
 def generate_configs_xgboost(num_random_configs=200):
-    config_generator = ConfigGenerator(name=name, manual_configs=manual_configs, search_space=search_space)
-    return config_generator.generate_all_configs(num_random_configs=num_random_configs)
+    search_space = ConfigurationSpace(
+        space=[
+            Float("learning_rate", (5e-3, 1e-1), log=True),
+            Integer("max_depth", (4, 10), log=True),
+            Float("min_child_weight", (1e-3, 5.0), log=True),
+            Float("subsample", (0.6, 1.0)),
+            Float("colsample_bylevel", (0.6, 1.0)),
+            Float("colsample_bynode", (0.6, 1.0)),
+            Float("reg_alpha", (1e-4, 5.0)),
+            Float("reg_lambda", (1e-4, 5.0)),
+            Categorical("grow_policy", ["depthwise", "lossguide"]),
+            Integer("max_cat_to_onehot", (8, 100), log=True),
+            Integer("max_leaves", (8, 1024), log=True),
+            # todo: do we still need to set enable_categorical?
+            # could search max_bin and num_parallel_tree but this is expensive
+        ],
+        seed=1234,
+    )
+
+    configs = search_space.sample_configuration(num_random_configs)
+    if num_random_configs == 1:
+        configs = [configs]
+    configs = [dict(config) for config in configs]
+    for c in configs:
+        c["enable_categorical"] = True
+    return [convert_numpy_dtypes(config) for config in configs]
+
+
+gen_xgboost = CustomAGConfigGenerator(
+    model_cls=XGBoostModel, search_space_func=generate_configs_xgboost
+)
+
+if __name__ == "__main__":
+    print(generate_configs_xgboost(3))
