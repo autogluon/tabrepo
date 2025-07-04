aa55a51de27cdd06022c7a85f5dd568ea6989a96
Lennart Purucker <contact@lennart-purucker.com>
2025-07-04 12:44:17 +0200

    fix/maint: add more subsets to end-to-end and ruffing

--- tabrepo/nips2025_utils/end_to_end.py
+++ tabrepo/nips2025_utils/end_to_end.py
@@ -1,17 +1,19 @@
 from __future__ import annotations
 
-import io
 from pathlib import Path
-
-import pandas as pd
+from typing import TYPE_CHECKING
 from typing_extensions import Self
 
-from tabrepo.benchmark.result import BaselineResult
+import pandas as pd
+
 from tabrepo.nips2025_utils.artifacts.method_metadata import MethodMetadata
 from tabrepo.nips2025_utils.method_processor import generate_task_metadata, load_raw
-from tabrepo.repository import EvaluationRepository
 from tabrepo.nips2025_utils.tabarena_context import TabArenaContext
 from tabrepo.paper.tabarena_evaluator import TabArenaEvaluator
+
+if TYPE_CHECKING:
+    from tabrepo.benchmark.result import BaselineResult
+    from tabrepo.repository import EvaluationRepository
 
 
 class EndToEnd:
@@ -20,16 +22,20 @@ class EndToEnd:
 
         # raw
         self.results_lst: list[BaselineResult] = results_lst
-        self.method_metadata: MethodMetadata = MethodMetadata.from_raw(results_lst=self.results_lst)
+        self.method_metadata: MethodMetadata = MethodMetadata.from_raw(
+            results_lst=self.results_lst
+        )
 
         if cache:
-            print(f'Artifacts for method {self.method_metadata.method} will be saved in: "{self.method_metadata.path}"')
+            print(
+                f'Artifacts for method {self.method_metadata.method} will be saved in: "{self.method_metadata.path}"'
+            )
 
         if cache:
             self.method_metadata.to_yaml()
             self.method_metadata.cache_raw(results_lst=self.results_lst)
 
-        tids = list(set(r.task_metadata["tid"] for r in self.results_lst))
+        tids = list({r.task_metadata["tid"] for r in self.results_lst})
         self.task_metadata = generate_task_metadata(tids=tids)
 
         # processed
@@ -53,10 +59,12 @@ class EndToEnd:
         return cls(results_lst=results_lst)
 
     @classmethod
-    def from_cache(cls, method: str, artifact_name: str = None) -> Self:
+    def from_cache(cls, method: str, artifact_name: str | None = None) -> Self:
         if artifact_name is None:
             artifact_name = method
-        method_metadata = MethodMetadata.from_yaml(method=method, artifact_name=artifact_name)
+        method_metadata = MethodMetadata.from_yaml(
+            method=method, artifact_name=artifact_name
+        )
         results_lst = method_metadata.load_raw()
         return cls(results_lst=results_lst)
 
@@ -77,20 +85,24 @@ class EndToEndResults:
         self.model_results = model_results
 
     @classmethod
-    def from_cache(cls, method: str, artifact_name: str = None) -> Self:
+    def from_cache(cls, method: str, artifact_name: str | None = None) -> Self:
         if artifact_name is None:
             artifact_name = method
-        method_metadata = MethodMetadata.from_yaml(method=method, artifact_name=artifact_name)
+        method_metadata = MethodMetadata.from_yaml(
+            method=method, artifact_name=artifact_name
+        )
         return cls(method_metadata=method_metadata)
 
-    def compare_on_tabarena(self, output_dir: str | Path, *, subset: str | None = None) -> pd.DataFrame:
-        """"Compare results on TabArena leaderboard.
+    def compare_on_tabarena(
+        self, output_dir: str | Path, *, subset: str | None = None
+    ) -> pd.DataFrame:
+        """Compare results on TabArena leaderboard.
 
         Args:
             output_dir (str | Path): Directory to save the results.
             subset (str | None): Subset of tasks to evaluate on.
-                Options are "classification", "regression", "lite" for TabArena-Lite,
-                or None for all tasks.
+                Options are "classification", "regression", "lite"  for TabArena-Lite,
+                "tabicl", "tabpfn", "tabpfn/tabicl", or None for all tasks.
         """
         output_dir = Path(output_dir)
 
@@ -99,26 +111,46 @@ class EndToEndResults:
         fillna_method = "RF (default)"
         paper_results = tabarena_context.load_results_paper(download_results="auto")
 
-
-
-
         # FIXME: Nick: After imputing: ta_name, ta_suite, config_type, etc. are incorrect,
         #  need to use original, not filled values
         #  This doesn't impact the evaluation, but could introduce bugs in future if we use these columns
         #  Fixing this is do-able, but requires some complex pandas tricks, so I haven't had time to implement it yet
         hpo_results = TabArenaContext.fillna_metrics(
             df_metrics=self.hpo_results,
-            df_fillna=paper_results[paper_results["method"] == fillna_method]
+            df_fillna=paper_results[paper_results["method"] == fillna_method],
         )
 
         df_results = pd.concat([paper_results, hpo_results], ignore_index=True)
 
-        if subset == "classification":
-            df_results = df_results[df_results["problem_type"].isin(["binary","multiclass"])].reset_index(drop=True)
-        elif subset == "regression":
-            df_results = df_results[df_results["problem_type"] == "regression"].reset_index(drop=True)
-        elif "lite":
-            df_results = df_results[df_results["fold"] == 0].reset_index(drop=True)
+        if subset is not None:
+            from tabrepo.nips2025_utils.fetch_metadata import load_task_metadata
+
+            if subset == "classification":
+                df_results = df_results[
+                    df_results["problem_type"].isin(["binary", "multiclass"])
+                ]
+            elif subset == "regression":
+                df_results = df_results[df_results["problem_type"] == "regression"]
+            elif subset == "lite":
+                df_results = df_results[df_results["fold"] == 0]
+            elif subset == "tabicl":
+                allowed_dataset = load_task_metadata(subset="TabICL")[
+                    "dataset"
+                ].tolist()
+                df_results = df_results[df_results["dataset"].isin(allowed_dataset)]
+            elif subset == "tabpfn":
+                allowed_dataset = load_task_metadata(subset="TabPFNv2")[
+                    "dataset"
+                ].tolist()
+                df_results = df_results[df_results["dataset"].isin(allowed_dataset)]
+            elif subset == "tabpfn/tabicl":
+                ad_tabicl = load_task_metadata(subset="TabICL")["dataset"].tolist()
+                ad_tabpfn = load_task_metadata(subset="TabPFNv2")["dataset"].tolist()
+                allowed_dataset = list(set(ad_tabicl).intersection(set(ad_tabpfn)))
+                df_results = df_results[df_results["dataset"].isin(allowed_dataset)]
+            else:
+                raise ValueError(f"Invalid subset {subset} name!")
+            df_results = df_results.reset_index(drop=True)
 
         # Handle imputation of names
         imputed_names = list(df_results["method"][df_results["imputed"] > 0].unique())
@@ -126,6 +158,7 @@ class EndToEndResults:
             imputed_names = None
         if imputed_names is not None:
             from tabrepo.paper.paper_utils import get_method_rename_map
+
             # remove suffix
             imputed_names = [n.split(" (")[0] for n in imputed_names]
             imputed_names = [get_method_rename_map().get(n, n) for n in imputed_names]
@@ -137,11 +170,10 @@ class EndToEndResults:
         plotter = TabArenaEvaluator(
             output_dir=output_dir,
         )
-        leaderboard = plotter.eval(
+        return plotter.eval(
             df_results=df_results,
             plot_extra_barplots=False,
             plot_times=True,
             plot_other=False,
             imputed_names=imputed_names,
         )
-        return leaderboard
