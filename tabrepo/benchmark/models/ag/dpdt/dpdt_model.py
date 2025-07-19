6dbe698537c8fc6af91c0bc4b93176c336e24b36
LennartPurucker <contact@lennart-purucker.com>
2025-07-19 21:29:10 +0200

    add: preprocessing for nan and cat handling

--- tabrepo/benchmark/models/ag/dpdt/dpdt_model.py
+++ tabrepo/benchmark/models/ag/dpdt/dpdt_model.py
@@ -10,9 +10,17 @@ if TYPE_CHECKING:
     import pandas as pd
 
 
+def _to_cat(X):
+    return X
+
+
 class BoostedDPDTModel(AbstractModel):
     ag_key = "BOOSTEDDPDT"
     ag_name = "boosted_dpdt"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._preprocessor = None
 
     def get_model_cls(self):
         from dpdt import AdaBoostDPDT
@@ -22,6 +30,55 @@ class BoostedDPDTModel(AbstractModel):
         else:
             raise AssertionError(f"Unsupported problem_type: {self.problem_type}")
         return model_cls
+
+    def _preprocess(self, X, **kwargs):
+        X = super()._preprocess(X, **kwargs)
+        if self._preprocessor is None:
+            import numpy as np
+            from sklearn.compose import ColumnTransformer, make_column_selector
+            from sklearn.impute import SimpleImputer
+            from sklearn.pipeline import Pipeline
+            from sklearn.preprocessing import FunctionTransformer, OrdinalEncoder
+
+            categorical_pipeline = Pipeline(
+                [
+                    (
+                        "encoder",
+                        OrdinalEncoder(
+                            handle_unknown="use_encoded_value", unknown_value=-1
+                        ),
+                    ),
+                    (
+                        "imputer",
+                        SimpleImputer(
+                            strategy="constant", add_indicator=True, fill_value=-1
+                        ),
+                    ),
+                    (
+                        "to_category",
+                        FunctionTransformer(_to_cat),
+                    ),
+                ]
+            ).set_output(transform="pandas")
+
+            self._preprocessor = ColumnTransformer(
+                transformers=[
+                    (
+                        "num",
+                        SimpleImputer(strategy="mean", add_indicator=True),
+                        make_column_selector(dtype_include=np.number),
+                    ),
+                    (
+                        "cat",
+                        categorical_pipeline,
+                        make_column_selector(dtype_include=["object", "category"]),
+                    ),
+                ],
+                remainder="passthrough",
+            ).set_output(transform="pandas")
+            self._preprocessor.fit(X)
+
+        return self._preprocessor.transform(X)
 
     def _fit(
         self,
@@ -86,12 +143,12 @@ class BoostedDPDTModel(AbstractModel):
 
         dataset_size_mem_est = (
             10
-            * hyperparameters.get("cart_nodes_list")[0]
+            * hyperparameters.get("cart_nodes_list", [2.5])[0]
             * get_approximate_df_mem_usage(X).sum()
         )
         baseline_overhead_mem_est = 3e8  # 300 MB generic overhead
 
-        return dataset_size_mem_est + baseline_overhead_mem_est
+        return int(dataset_size_mem_est + baseline_overhead_mem_est)
 
     @classmethod
     def _class_tags(cls):
