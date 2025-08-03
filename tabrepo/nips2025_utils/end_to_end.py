f5459d659e83422b92a107b72973865d48a50361
LennartPurucker <contact@lennart-purucker.com>
2025-08-03 16:09:25 +0200

    add: prototype to compare multiple models

--- tabrepo/nips2025_utils/end_to_end.py
+++ tabrepo/nips2025_utils/end_to_end.py
@@ -242,3 +242,118 @@ class EndToEndResults:
             plot_other=False,
             imputed_names=imputed_names,
         )
+
+    def get_data_to_compare_on_tabarena(
+        self,
+        *,
+        new_result_prefix: str | None = None,
+    ) -> pd.DataFrame:
+        """Get data to compare results on TabArena leaderboard.
+
+        Args:
+            new_result_prefix (str | None): If not None, add a prefix to the new
+                results to distinguish new results from the original TabArena results.
+                Use this, for example, if you re-run a model from TabArena.
+        """
+        tabarena_context = TabArenaContext()
+
+        fillna_method = "RF (default)"
+        paper_results = tabarena_context.load_results_paper(download_results="auto")
+
+        if new_result_prefix is not None:
+            for col in ["method", "config_type", "ta_name", "ta_suite"]:
+                self.hpo_results[col] = new_result_prefix + self.hpo_results[col]
+
+        # FIXME: Nick: After imputing: ta_name, ta_suite, config_type, etc. are incorrect,
+        #  need to use original, not filled values
+        #  This doesn't impact the evaluation, but could introduce bugs in future if we use these columns
+        #  Fixing this is do-able, but requires some complex pandas tricks, so I haven't had time to implement it yet
+        return TabArenaContext.fillna_metrics(
+            df_metrics=self.hpo_results,
+            df_fillna=paper_results[paper_results["method"] == fillna_method],
+        )
+
+
+
+def compare_on_tabarena(
+    new_results: list[pd.DataFrame],
+    output_dir: str | Path,
+    *,
+    subset: str | None | list = None,
+) -> pd.DataFrame:
+    """Compare results on TabArena leaderboard.
+
+    Args:
+        new_results: List of new results DataFrames to compare.
+        output_dir (str | Path): Directory to save the results.
+        subset (str | None | list): Subset of tasks to evaluate on.
+            Options are "classification", "regression", "lite"  for TabArena-Lite,
+            "tabicl", "tabpfn", "tabpfn/tabicl", or None for all tasks.
+            Or a list of subset names to filter for.
+    """
+    output_dir = Path(output_dir)
+
+    tabarena_context = TabArenaContext()
+    paper_results = tabarena_context.load_results_paper(download_results="auto")
+
+    df_results = pd.concat([paper_results, *new_results], ignore_index=True)
+
+    if subset is not None:
+        from tabrepo.nips2025_utils.fetch_metadata import load_task_metadata
+
+        if isinstance(subset, str):
+            subset = [subset]
+
+        for filter_subset in subset:
+            if filter_subset == "classification":
+                df_results = df_results[
+                    df_results["problem_type"].isin(["binary", "multiclass"])
+                ]
+            elif filter_subset == "regression":
+                df_results = df_results[df_results["problem_type"] == "regression"]
+            elif filter_subset == "lite":
+                df_results = df_results[df_results["fold"] == 0]
+            elif filter_subset == "tabicl":
+                allowed_dataset = load_task_metadata(subset="TabICL")[
+                    "dataset"
+                ].tolist()
+                df_results = df_results[df_results["dataset"].isin(allowed_dataset)]
+            elif filter_subset == "tabpfn":
+                allowed_dataset = load_task_metadata(subset="TabPFNv2")[
+                    "dataset"
+                ].tolist()
+                df_results = df_results[df_results["dataset"].isin(allowed_dataset)]
+            elif filter_subset == "tabpfn/tabicl":
+                ad_tabicl = load_task_metadata(subset="TabICL")["dataset"].tolist()
+                ad_tabpfn = load_task_metadata(subset="TabPFNv2")["dataset"].tolist()
+                allowed_dataset = list(set(ad_tabicl).intersection(set(ad_tabpfn)))
+                df_results = df_results[df_results["dataset"].isin(allowed_dataset)]
+            else:
+                raise ValueError(f"Invalid subset {subset} name!")
+            df_results = df_results.reset_index(drop=True)
+
+    # Handle imputation of names
+    imputed_names = list(df_results["method"][df_results["imputed"] > 0].unique())
+    if len(imputed_names) == 0:
+        imputed_names = None
+    if imputed_names is not None:
+        from tabrepo.paper.paper_utils import get_method_rename_map
+
+        # remove suffix
+        imputed_names = [n.split(" (")[0] for n in imputed_names]
+        imputed_names = [get_method_rename_map().get(n, n) for n in imputed_names]
+        imputed_names = list(set(imputed_names))
+        if "KNN" in imputed_names:
+            imputed_names.remove("KNN")
+        print(f"Model for which results were imputed: {imputed_names}")
+
+    plotter = TabArenaEvaluator(
+        output_dir=output_dir,
+    )
+    return plotter.eval(
+        df_results=df_results,
+        plot_extra_barplots=False,
+        plot_times=True,
+        plot_other=False,
+        imputed_names=imputed_names,
+    )
