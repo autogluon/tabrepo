2155d4f1c7f778abd08b43f28e4113f5ed91d008
Paul Koch <46825734+paulbkoch@users.noreply.github.com>
2025-08-11 23:59:36 -0700

    Update EBM method and hyperparameters (#158)
    Co-authored-by: LennartPurucker <contact@lennart-purucker.com>

--- tabrepo/benchmark/models/ag/ebm/ebm_model.py
+++ tabrepo/benchmark/models/ag/ebm/ebm_model.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
+import time
 import warnings
 from typing import TYPE_CHECKING
 
 import numpy as np
 import pandas as pd
-from autogluon.common.utils.pandas_utils import get_approximate_df_mem_usage
 from autogluon.core.constants import BINARY, MULTICLASS, REGRESSION
 from autogluon.core.models import AbstractModel
 
@@ -13,38 +13,23 @@ if TYPE_CHECKING:
     from autogluon.core.metrics import Scorer
 
 
-# TODO: support time limit
-# TODO: hyperparameters and good defaults
-# TODO: memory usage estimation
-# TODO: fix joblib errors when using EBM
-# TODO: handle interactions for multiclass
+class EbmCallback:
+    """Time limit callback for EBM."""
+
+    def __init__(self, seconds: int):
+        self.seconds = seconds
+        self.end_time: int | None = None
+
+    def __call__(self, **kwargs):
+        if self.end_time is None:
+            self.end_time = time.monotonic() + self.seconds
+            return False
+        return time.monotonic() > self.end_time
+
+
 class ExplainableBoostingMachineModel(AbstractModel):
     ag_key = "EBM"
     ag_name = "ExplainableBM"
-
-    _category_features: list[str] = None
-
-    def _get_model_type(self):
-        match self.problem_type:
-            case _ if self.problem_type in (BINARY, MULTICLASS):
-                from interpret.glassbox import ExplainableBoostingClassifier
-
-                model_cls = ExplainableBoostingClassifier
-            case _ if self.problem_type == REGRESSION:
-                from interpret.glassbox import ExplainableBoostingRegressor
-
-                model_cls = ExplainableBoostingRegressor
-            case _:
-                raise ValueError(f"Unsupported problem type: {self.problem_type}")
-        return model_cls
-
-    def _preprocess_nonadaptive(self, X, **kwargs):
-        X = super()._preprocess_nonadaptive(X, **kwargs)
-
-        if self._category_features is None:
-            self._category_features = list(X.select_dtypes(include="category").columns)
-
-        return X
 
     def _fit(
         self,
@@ -52,92 +37,71 @@ class ExplainableBoostingMachineModel(AbstractModel):
         y: pd.Series,
         X_val: pd.DataFrame | None = None,
         y_val: pd.Series | None = None,
+        time_limit: float | None = None,
         sample_weight: np.ndarray | None = None,
         sample_weight_val: np.ndarray | None = None,
         num_cpus: int | str = "auto",
         **kwargs,
     ):
-        # Create validation set if not provided to enable early stopping.
-        if X_val is None:
-            from autogluon.core.utils import generate_train_test_split
-
-            X, X_val, y, y_val = generate_train_test_split(
-                X=X,
-                y=y,
-                problem_type=self.problem_type,
-                test_size=0.2,
-                random_state=0,
-            )
-
         # Preprocess data.
-        X = self.preprocess(X, is_train=True)
-        X_val = self.preprocess(X_val, is_train=False)
-        paras = self._get_model_params()
+        X = self.preprocess(X)
+        if X_val is not None:
+            X_val = self.preprocess(X_val)
 
-        # Handle categorical column types ordinal and nominal columns.
-        ordinal_columns = paras.pop("ordinal_columns", [])  # The user can specify ordinal columns.
-        nominal_columns = paras.pop("nominal_columns", [])  # The user can specify nominal columns.
-        feature_types = []
-        for c in self._features:
-            if c in ordinal_columns:
-                f_type = "ordinal"
-            elif c in nominal_columns:
-                f_type = "nominal"
-            elif c in self._category_features:
-                # Fallback if user did not specify column type.
-                f_type = "nominal"
-            else:
-                f_type = "auto"
-            feature_types.append(f_type)
+        features = self._features
+        if features is None:
+            features = X.columns
 
-        # Default parameters for EBM
-        extra_kwargs = dict(
-            outer_bags=1,  # AutoGluon ensemble creates outer bags, no need for this overhead.
-            inner_bags=0,  # We supply the validation set, no need for inner bags.
-            objective=get_metric_from_ag_metric(metric=self.stopping_metric, problem_type=self.problem_type),
-            feature_names=self._features,
-            n_jobs=-1 if isinstance(num_cpus, str) else num_cpus,
+        params = construct_ebm_params(
+            self.problem_type,
+            self._get_model_params(),
+            features,
+            self.stopping_metric,
+            num_cpus,
+            time_limit,
         )
-        extra_kwargs.update(paras)
 
         # Init Class
-        model_cls = self._get_model_type()
-        self.model = model_cls(**extra_kwargs)
+        model_cls = get_class_from_problem_type(self.problem_type)
+        self.model = model_cls(random_state=self.random_seed, **params)
 
         # Handle validation data format for EBM
-        fit_X = pd.concat([X, X_val], ignore_index=True)
-        fit_y = pd.concat([y, y_val], ignore_index=True)
-        bag = np.full(len(fit_X), 1)
-        bag[len(X) :] = -1
-
-        # Sample Weights
-        fit_sample_weight = np.hstack([sample_weight, sample_weight_val]) if sample_weight is not None else None
+        fit_X = X
+        fit_y = y
+        fit_sample_weight = sample_weight
+        bags = None
+        if X_val is not None:
+            fit_X = pd.concat([X, X_val], ignore_index=True)
+            fit_y = pd.concat([y, y_val], ignore_index=True)
+            if sample_weight is not None:
+                fit_sample_weight = np.hstack([sample_weight, sample_weight_val])
+            bags = np.full((len(fit_X), 1), 1, np.int8)
+            bags[len(X) :, 0] = -1
 
         with warnings.catch_warnings():  # try to filter joblib warnings
-            warnings.filterwarnings("ignore", category=UserWarning, message=".*resource_tracker: process died.*")
-            self.model.fit(fit_X, fit_y, sample_weight=fit_sample_weight, bags=[bag])
+            warnings.filterwarnings(
+                "ignore",
+                category=UserWarning,
+                message=".*resource_tracker: process died.*",
+            )
+            self.model.fit(fit_X, fit_y, sample_weight=fit_sample_weight, bags=bags)
+
+    def _get_random_seed_from_hyperparameters(
+        self, hyperparameters: dict
+    ) -> int | None | str:
+        return hyperparameters.get("random_state", "N/A")
 
     def _get_default_auxiliary_params(self) -> dict:
         default_auxiliary_params = super()._get_default_auxiliary_params()
-        extra_auxiliary_params = dict(
-            valid_raw_types=["int", "float", "category"],
-        )
+        extra_auxiliary_params = {
+            "valid_raw_types": ["int", "float", "category"],
+        }
         default_auxiliary_params.update(extra_auxiliary_params)
         return default_auxiliary_params
 
     @classmethod
     def supported_problem_types(cls) -> list[str] | None:
         return ["binary", "multiclass", "regression"]
-
-    # FIXME: Find a better estimate for memory usage of EBM.
-    @classmethod
-    def _estimate_memory_usage_static(
-        cls,
-        *,
-        X: pd.DataFrame,
-        **kwargs,
-    ) -> int:
-        return 5 * get_approximate_df_mem_usage(X).sum()
 
     @classmethod
     def _class_tags(cls) -> dict:
@@ -147,41 +111,118 @@ class ExplainableBoostingMachineModel(AbstractModel):
         """EBMs do not yet support refit full."""
         return {"can_refit_full": False}
 
+    def _estimate_memory_usage(self, X: pd.DataFrame, **kwargs) -> int:
+        return self.estimate_memory_usage_static(
+            X=X,
+            problem_type=self.problem_type,
+            num_classes=self.num_classes,
+            hyperparameters=self._get_model_params(),
+            features=self._features,
+            **kwargs,
+        )
+
+    @classmethod
+    def _estimate_memory_usage_static(
+        cls,
+        *,
+        X: pd.DataFrame,
+        problem_type: str,
+        hyperparameters: dict | None = None,
+        num_classes: int = 1,
+        features=None,
+        **kwargs,
+    ) -> int:
+        """Returns the expected peak memory usage in bytes of the EBM model during fit."""
+        # TODO: we can improve the memory estimate slightly by using num_classes
+
+        if features is None:
+            features = X.columns
+
+        params = construct_ebm_params(problem_type, hyperparameters, features)
+
+        model_cls = get_class_from_problem_type(problem_type)
+
+        baseline_memory_bytes = 400_000_000  # 400 MB baseline memory
+
+        # assuming we call pd.concat([X, X_val], ignore_index=True) above, then it will be doubled
+        return baseline_memory_bytes + model_cls(**params).estimate_mem(
+            X, data_multiplier=2.0
+        )
+
+    def _validate_fit_memory_usage(self, mem_error_threshold: float = 1, **kwargs):
+        # Given the good mem estimates with overhead, we set the threshold to 1.
+        return super()._validate_fit_memory_usage(
+            mem_error_threshold=mem_error_threshold, **kwargs
+        )
+
+
+def construct_ebm_params(
+    problem_type,
+    hyperparameters=None,
+    features=None,
+    stopping_metric=None,
+    num_cpus=-1,
+    time_limit=None,
+):
+    if hyperparameters is None:
+        hyperparameters = {}
+
+    hyperparameters = hyperparameters.copy()  # we pop values below, so copy.
+
+    # The user can specify nominal and continuous columns.
+    continuous_columns = hyperparameters.pop("continuous_columns", [])
+    nominal_columns = hyperparameters.pop("nominal_columns", [])
+
+    feature_types = None
+    if features is not None:
+        feature_types = []
+        for c in features:
+            if c in continuous_columns:
+                f_type = "continuous"
+            elif c in nominal_columns:
+                f_type = "nominal"
+            else:
+                f_type = "auto"
+            feature_types.append(f_type)
+
+    # Default parameters for EBM
+    params = {
+        "outer_bags": 1,  # AutoGluon ensemble creates outer bags, no need for this overhead.
+        "feature_names": features,
+        "feature_types": feature_types,
+        "n_jobs": -1 if isinstance(num_cpus, str) else num_cpus,
+    }
+    if stopping_metric is not None:
+        params["objective"] = get_metric_from_ag_metric(
+            metric=stopping_metric, problem_type=problem_type
+        )
+    if time_limit is not None:
+        params["callback"] = EbmCallback(time_limit)
+
+    params.update(hyperparameters)
+    return params
+
+
+def get_class_from_problem_type(problem_type: str):
+    if problem_type in [BINARY, MULTICLASS]:
+        from interpret.glassbox import ExplainableBoostingClassifier
+
+        model_cls = ExplainableBoostingClassifier
+    elif problem_type == REGRESSION:
+        from interpret.glassbox import ExplainableBoostingRegressor
+
+        model_cls = ExplainableBoostingRegressor
+    else:
+        raise ValueError(f"Unsupported problem type: {problem_type}")
+    return model_cls
+
 
 def get_metric_from_ag_metric(*, metric: Scorer, problem_type: str):
     """Map AutoGluon metric to EBM metric for early stopping."""
-    if problem_type == BINARY:
-        metric_map = dict(
-            log_loss="log_loss",
-            accuracy="log_loss",
-            roc_auc="log_loss",
-            f1="log_loss",
-            f1_macro="log_loss",
-            f1_micro="log_loss",
-            f1_weighted="log_loss",
-            balanced_accuracy="log_loss",
-            recall="log_loss",
-            recall_macro="log_loss",
-            recall_micro="log_loss",
-            recall_weighted="log_loss",
-            precision="log_loss",
-            precision_macro="log_loss",
-            precision_micro="log_loss",
-            precision_weighted="log_loss",
-        )
-        metric_class = metric_map.get(metric.name, "log_loss")
-    elif problem_type == MULTICLASS:
-        metric_map = dict(log_loss="log_loss", accuracy="log_loss", roc_auc_ovo_macro="log_loss")
-        metric_class = metric_map.get(metric.name, "log_loss")
+    if problem_type in [BINARY, MULTICLASS]:
+        metric_class = "log_loss"
     elif problem_type == REGRESSION:
-        metric_map = dict(
-            mean_squared_error="rmse",
-            root_mean_squared_error="rmse",
-            mean_absolute_error="rmse",
-            median_absolute_error="rmse",
-            r2="rmse",  # rmse_log maybe?
-        )
-        metric_class = metric_map.get(metric.name, "rmse")
+        metric_class = "rmse"
     else:
         raise AssertionError(f"EBM does not support {problem_type} problem type.")
 
--- tabrepo/utils/config_utils.py
+++ tabrepo/utils/config_utils.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import copy
-from typing import Type
+from typing import Type, Literal
 
 from autogluon.core.searcher.local_random_searcher import LocalRandomSearcher
 from autogluon.core.models import AbstractModel
@@ -40,6 +40,13 @@ def add_suffix_to_config(config, suffix):
     config['ag_args'] = {'name_suffix': suffix}
     return config
 
+def add_seed_logic(config: dict, random_seed: int, vary_seed_across_folds: bool) -> dict:
+    config = copy.deepcopy(config)
+    if "ag_args_ensemble" not in config:
+        config["ag_args_ensemble"] = {}
+    config["ag_args_ensemble"]["model_random_seed"] = random_seed
+    config["ag_args_ensemble"]["vary_seed_across_folds"] = vary_seed_across_folds
+    return config
 
 def get_random_searcher(search_space):
     searcher = LocalRandomSearcher(search_space=search_space)
@@ -86,9 +93,9 @@ class AGConfigGenerator(AbstractConfigGenerator):
         configs = combine_manual_and_random_configs(manual_configs=self.manual_configs, random_configs=random_configs, name_id_suffix=name_id_suffix)
         return configs
 
-    def generate_all_bag_experiments(self, num_random_configs: int, name_id_suffix: str = "") -> list:
+    def generate_all_bag_experiments(self, num_random_configs: int, name_id_suffix: str = "", add_seed: Literal["static", "fold-wise", "fold-config-wise"] = "static") -> list:
         configs = self.generate_all_configs_lst(num_random_configs=num_random_configs, name_id_suffix=name_id_suffix)
-        experiments = generate_bag_experiments(model_cls=self.model_cls, configs=configs, name_suffix_from_ag_args=True)
+        experiments = generate_bag_experiments(model_cls=self.model_cls, configs=configs, name_suffix_from_ag_args=True, add_seed=add_seed)
         return experiments
 
 
@@ -155,11 +162,33 @@ def generate_bag_experiments(
     name_id_suffix: str = "",
     name_bag_suffix: str = "_BAG_L1",
     add_name_suffix_to_params: bool = True,
-    **kwargs
+    add_seed: Literal["static", "fold-wise", "fold-config-wise"] = "static",
+    **kwargs,
 ) -> list[AGModelBagExperiment]:
     experiments = []
     if kwargs is None:
         kwargs = {}
+
+    if add_seed == "static":
+        configs = [
+            add_seed_logic(config=config, random_seed=0, vary_seed_across_folds=False)
+            for config in configs
+        ]
+    elif add_seed == "fold-wise":
+        configs = [
+            add_seed_logic(config=config, random_seed=0, vary_seed_across_folds=True)
+            for config in configs
+        ]
+    elif add_seed == "fold-config-wise":
+        offset_between_configs = num_bag_sets * num_bag_folds
+        configs = [
+            add_seed_logic(config=config, random_seed=i * offset_between_configs, vary_seed_across_folds=True)
+            for i, config in enumerate(configs)
+        ]
+    else:
+        raise ValueError(
+            f"Invalid add_seed value: {add_seed}. Choose from 'static', 'fold-wise', or 'fold-config-wise'."
+        )
 
     for i, config in enumerate(configs):
         if name_suffix_from_ag_args:
