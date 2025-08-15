eee308d082e13fb39c9e6bb867f441740bd258f4
David Holzmüller <david.holzmueller@online.de>
2025-08-16 00:54:17 +0200

    xrfm: added memory estimation, corrected standardization preprocessing, standardize targets

--- tabrepo/benchmark/models/ag/xrfm/xrfm_model.py
+++ tabrepo/benchmark/models/ag/xrfm/xrfm_model.py
@@ -9,6 +9,7 @@ from typing import Literal
 
 import numpy as np
 import pandas as pd
+from autogluon.core.constants import REGRESSION
 from sklearn.impute import SimpleImputer
 
 from autogluon.common.utils.pandas_utils import get_approximate_df_mem_usage
@@ -29,6 +30,94 @@ def set_logger_level(logger_name: str, level: int):
         yield
     finally:
         _logger.setLevel(old_level)
+
+
+class xRFMImplementation:
+    def __init__(self, problem_type, **kwargs):
+        self.problem_type = problem_type
+        self.kwargs = kwargs
+
+    def fit(self, X, y, X_val, y_val):
+        try:
+            import xrfm
+            import torch
+        except ImportError as err:
+            logger.log(
+                40,
+                f"\tFailed to import xrfm/torch! To use the xRFM model, "
+                f"do: `pip install autogluon.tabular[xrfm]=={__version__}`.",
+            )
+            raise err
+
+        # preprocessing
+
+        self.cat_cols_ = X.select_dtypes(include="category").columns.tolist()
+        self.num_cols_ = X.select_dtypes(exclude="category").columns.tolist()
+        # todo: 'ignore' may be problematic for the automatic categorical handling of xRFM?
+        self.ohe_ = OneHotEncoder(handle_unknown='ignore')
+        self.std_ = StandardScaler()
+        x_cat_enc = self.ohe_.fit_transform(X.loc[:, self.cat_cols_])
+        x_num_enc = X.loc[:, self.num_cols_].to_numpy().astype(np.float32)
+        X = np.concatenate([x_cat_enc, x_num_enc], axis=1)
+        X = self.std_.fit_transform(X)
+
+        # compute categorical info
+        idx = 0
+        categorical_indices = []
+        categorical_vectors = []
+        for cat in self.ohe_.categories_:
+            cat_idxs = torch.tensor(range(idx, idx + len(cat)))
+            categorical_indices.append(cat_idxs)
+            cat_vectors = (np.asarray(self.std_.scale_)[None, cat_idxs.numpy()] *
+                           (np.eye(len(cat)) - np.asarray(self.std_.mean_)[None, cat_idxs.numpy()]))
+            categorical_vectors.append(torch.tensor(cat_vectors, dtype=torch.float32))
+            idx += len(cat)
+        # numerical indices are after the categoricals
+        numerical_indices = idx + torch.arange(x_num_enc.shape[1])
+        self.categorical_info_ = dict(
+            categorical_indices=categorical_indices,
+            categorical_vectors=categorical_vectors,
+            numerical_indices=numerical_indices,
+        )
+
+        if self.problem_type == REGRESSION:
+            # standardize regression target (may not be necessary)
+            y = np.asarray(y)
+            self.mean_ = np.mean(y, axis=0)
+            self.std_ = np.std(y, axis=0) + 1e-30
+            y = (y - self.mean_) / self.std_
+            if y_val is not None:
+                y_val = np.asarray(y_val)
+                y_val = (y_val - self.mean_) / self.std_
+
+        self.model = xrfm.xRFM(
+            categorical_info=self.categorical_info_,  # set in preprocess()
+            **self.kwargs,
+        )
+
+        self.model.fit(
+            X=X,
+            y=y,
+            X_val=X_val,
+            y_val=y_val,
+        )
+
+    def preprocess_transform(self, X):
+        x_cat_enc = self.ohe_.transform(X.loc[:, self.cat_cols_])
+        x_num_enc = X.loc[:, self.num_cols_].to_numpy().astype(np.float32)
+        X = np.concatenate([x_cat_enc, x_num_enc], axis=1)
+        X = self.std_.transform(X)
+        return X
+
+    def predict(self, X):
+        y_pred = self.model.predict(self.preprocess_transform(X))
+        if self.task_type == REGRESSION:
+            y_pred = y_pred * self.std_ + self.mean_
+        return y_pred
+
+    def predict_proba(self, X):
+        return self.model.predict_proba(self.preprocess_transform(X))
+
 
 
 # pip install xrfm
@@ -58,12 +147,11 @@ class xRFMModel(AbstractModel):
         start_time = time.time()
 
         try:
-            import xrfm
             import torch
         except ImportError as err:
             logger.log(
                 40,
-                f"\tFailed to import xrfm/torch! To use the xRFM model, "
+                f"\tFailed to import torch! To use the xRFM model, "
                 f"do: `pip install autogluon.tabular[xrfm]=={__version__}`.",
             )
             raise err
@@ -97,7 +185,7 @@ class xRFMModel(AbstractModel):
         if tuning_metric is not None:
             init_kwargs["tuning_metric"] = tuning_metric
 
-        bool_to_cat = hyp.pop("bool_to_cat", True)
+        bool_to_cat = hyp.pop("bool_to_cat", False)
         impute_bool = hyp.pop("impute_bool", True)
 
         init_kwargs['rfm_params'] = {
@@ -118,20 +206,19 @@ class xRFMModel(AbstractModel):
             }
         }
 
-
-
-        # todo: should we set default_rfm_params as well?
+        # todo: should we set default_rfm_params as well? -> I guess not?
+        # todo: set M_batch_size?
+        # todo: do we already need to move stuff to the correct device?
 
         X = self.preprocess(X, is_train=True, bool_to_cat=bool_to_cat, impute_bool=impute_bool)
 
         if X_val is not None:
             X_val = self.preprocess(X_val)
 
-        self.model = xrfm.xRFM(
+        self.model = xRFMImplementation(
             n_threads=num_cpus,
             device=device,
             time_limit_s=time_limit - (time.time() - start_time) if time_limit is not None else None,
-            categorical_info=self.categorical_info_,  # set in preprocess()
             **init_kwargs,
         )
 
@@ -181,66 +268,16 @@ class xRFMModel(AbstractModel):
             # FIXME: Use CategoryFeatureGenerator? Or tell the model which is category
             X[self._features_bool] = X[self._features_bool].astype("category")
 
-        import torch
-
-        if is_train:
-            self.cat_cols_ = X.select_dtypes(include="category").columns.tolist()
-            self.num_cols_ = X.select_dtypes(exclude="category").columns.tolist()
-            # todo: 'ignore' may be problematic for the automatic categorical handling of xRFM?
-            self.ohe_ = OneHotEncoder(handle_unknown='ignore')
-            self.std_ = StandardScaler()  # todo: is it correct to apply this only to numericals?
-            x_cat_enc = self.ohe_.fit_transform(X.loc[:, self.cat_cols_])
-            x_num_enc = self.std_.fit_transform(X.loc[:, self.num_cols_].to_numpy().astype(np.float32))
-            X = np.concatenate([x_cat_enc, x_num_enc], axis=1)
-
-            # compute categorical info
-            idx = 0
-            categorical_indices = []
-            categorical_vectors = []
-            for cat in self.ohe_.categories_:
-                categorical_indices.append(torch.tensor(range(idx, idx+len(cat))))
-                categorical_vectors.append(torch.eye(len(cat)))
-                idx += len(cat)
-            # numerical indices are after the categoricals
-            numerical_indices = idx + torch.arange(x_num_enc.shape[1])
-            self.categorical_info_ = dict(
-                categorical_indices=categorical_indices,
-                categorical_vectors=categorical_vectors,
-                numerical_indices=numerical_indices,
-            )
-        else:
-            x_cat_enc = self.ohe_.transform(X.loc[:, self.cat_cols_])
-            x_num_enc = self.std_.transform(X.loc[:, self.num_cols_].to_numpy().astype(np.float32))
-            X = np.concatenate([x_cat_enc, x_num_enc], axis=1)
-
         return X
 
     def _set_default_params(self):
         default_params = dict(
             random_state=0,
 
-            # Don't use early stopping by default, seems to work well without
-            use_early_stopping=False,
-            early_stopping_additive_patience=40,
-            early_stopping_multiplicative_patience=3,
-
-            # verdict: use_ls="auto" is much better than None.
-            use_ls="auto",
-
-            # verdict: no impact, but makes more sense to be False.
+            # copied from RealMLP
             impute_bool=False,
-
-            # verdict: name_categories=True avoids random exceptions being raised in rare cases
             name_categories=True,
-
-            # verdict: bool_to_cat=True is equivalent to False in terms of quality, but can be slightly faster in training time
-            #  and slightly slower in inference time
-            bool_to_cat=True,
-
-            # verdict: "td" is better than "td_s"
-            default_hyperparameters="td",  # options ["td", "td_s"]
-
-            predict_batch_size="auto",  # if auto, uses AutoGluon's heuristic to set a value between 8192 and 64.
+            bool_to_cat=False,
         )
         for param, val in default_params.items():
             self._set_default_param_value(param, val)
@@ -273,54 +310,24 @@ class xRFMModel(AbstractModel):
         **kwargs,
     ) -> int:
         """
-        Heuristic memory estimate that correlates strongly with RealMLP's more sophisticated method
-
-        More comprehensive memory estimate logic:
-
-        ```python
-        from typing import Any
-
-        from pytabkit.models.alg_interfaces.nn_interfaces import NNAlgInterface
-        from pytabkit.models.data.data import DictDataset, TensorInfo
-        from pytabkit.models.sklearn.default_params import DefaultParams
-
-        def estimate_realmlp_cpu_ram_gb(hparams: dict[str, Any], n_numerical: int, cat_sizes: list[int], n_classes: int,
-                                        n_samples: int):
-            params = copy.copy(DefaultParams.RealMLP_TD_CLASS if n_classes > 0 else DefaultParams.RealMLP_TD_REG)
-            params.update(hparams)
-
-            ds = DictDataset(tensors=None, tensor_infos=dict(x_cont=TensorInfo(feat_shape=[n_numerical]),
-                                                             x_cat=TensorInfo(cat_sizes=cat_sizes),
-                                                             y=TensorInfo(cat_sizes=[n_classes])), device='cpu',
-                             n_samples=n_samples)
-
-            alg_interface = NNAlgInterface(**params)
-            res = alg_interface.get_required_resources(ds, n_cv=1, n_refit=0, n_splits=1, split_seeds=[0], n_train=n_samples)
-            return res.cpu_ram_gb
+        Heuristic memory estimate.
         ```
 
         """
         if hyperparameters is None:
             hyperparameters = {}
-        plr_hidden_1 = hyperparameters.get("plr_hidden_1", 16)
-        plr_hidden_2 = hyperparameters.get("plr_hidden_2", 4)
-        hidden_width = hyperparameters.get("hidden_width", 256)
 
         num_features = len(X.columns)
-        columns_mem_est = num_features * 8e5
+        num_samples = len(X)
 
-        hidden_1_weight = 0.13
-        hidden_2_weight = 0.42
-        width_factor = math.sqrt(hidden_width / 256 + 0.6)
-
-        columns_mem_est_hidden_1 = columns_mem_est * hidden_1_weight * plr_hidden_1 / 16 * width_factor
-        columns_mem_est_hidden_2 = columns_mem_est * hidden_2_weight * plr_hidden_2 / 16 * width_factor
-        columns_mem_est = columns_mem_est_hidden_1 + columns_mem_est_hidden_2
+        # taken from pytabkit
+        columns_mem_est = 6e4 * num_samples + 2e6 * num_features + 30.0 * num_samples**2 + 30.0 * num_samples * num_features
 
         dataset_size_mem_est = 5 * get_approximate_df_mem_usage(X).sum()  # roughly 5x DataFrame memory size
-        baseline_overhead_mem_est = 3e8  # 300 MB generic overhead
+        baseline_overhead_mem_est = 2e8  # 200 MB generic overhead
 
         mem_estimate = dataset_size_mem_est + columns_mem_est + baseline_overhead_mem_est
+        mem_estimate = min(mem_estimate, 3.8e10)  # using the tree strategy caps at <40 GB
 
         return mem_estimate
 
