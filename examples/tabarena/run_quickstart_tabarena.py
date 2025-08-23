38abf98f782ee060ef5681b4c042af25968e74dc
ubuntu <innixma@gmail.com>
2025-08-23 01:50:04 +0000

    Add WIP run_quickstart_tabarena.py

--- examples/tabarena/run_quickstart_tabarena.py
+++ examples/tabarena/run_quickstart_tabarena.py
@@ -5,19 +5,19 @@ from typing import Any
 
 import pandas as pd
 
-from tabrepo import EvaluationRepository, Evaluator
 from tabrepo.benchmark.experiment import AGModelBagExperiment, ExperimentBatchRunner
-from tabrepo.benchmark.result import ExperimentResults
-from tabrepo.nips2025_utils.fetch_metadata import load_task_metadata
+from tabrepo.nips2025_utils.end_to_end import EndToEnd
 from tabrepo.nips2025_utils.tabarena_context import TabArenaContext
+from tabrepo.tabarena.website_format import format_leaderboard
 
 
 if __name__ == '__main__':
     expname = str(Path(__file__).parent / "experiments" / "quickstart")  # folder location to save all experiment artifacts
-    repo_dir = str(Path(__file__).parent / "repos" / "quickstart")  # Load the repo later via `EvaluationRepository.from_dir(repo_dir)`
+    eval_dir = Path(__file__).parent / "eval" / "quickstart"
     ignore_cache = False  # set to True to overwrite existing caches and re-run experiments from scratch
 
-    task_metadata = load_task_metadata()
+    tabarena_context = TabArenaContext()
+    task_metadata = tabarena_context.task_metadata
 
     # Sample for a quick demo
     datasets = ["anneal", "credit-g", "diabetes"]  # datasets = list(task_metadata["name"])
@@ -55,67 +55,19 @@ if __name__ == '__main__':
         ignore_cache=ignore_cache,
     )
 
-    experiment_results = ExperimentResults(task_metadata=task_metadata)
+    # FIXME: Support multiple different methods in EndToEnd
+    # compute results
+    end_to_end = EndToEnd(results_lst=results_lst, task_metadata=task_metadata, cache=False)
+    end_to_end_results = end_to_end.to_results()
 
-    # Convert the run artifacts into an EvaluationRepository
-    repo: EvaluationRepository = experiment_results.repo_from_results(results_lst=results_lst)
-    repo.print_info()
-
-    repo.to_dir(path=repo_dir)  # Load the repo later via `EvaluationRepository.from_dir(repo_dir)`
-
-    print(f"New Configs Hyperparameters: {repo.configs_hyperparameters()}")
-
-    # get the local results
-    evaluator = Evaluator(repo=repo)
-    metrics: pd.DataFrame = evaluator.compare_metrics().reset_index().rename(columns={"framework": "method"})
-
-    # load the TabArena paper results
-    tabarena_context = TabArenaContext()
-    tabarena_results = tabarena_context.load_results_paper(download_results="auto")
-    tabarena_results = tabarena_results[[c for c in tabarena_results.columns if c in metrics.columns]]
-
-    dataset_fold_map = metrics.groupby("dataset")["fold"].apply(set)
-
-    def is_in(dataset: str, fold: int) -> bool:
-        return (dataset in dataset_fold_map.index) and (fold in dataset_fold_map.loc[dataset])
-
-    # filter tabarena_results to only the dataset, fold pairs that are present in `metrics`
-    is_in_lst = [is_in(dataset, fold) for dataset, fold in zip(tabarena_results["dataset"], tabarena_results["fold"])]
-    tabarena_results = tabarena_results[is_in_lst]
-
-    metrics = pd.concat([
-        metrics,
-        tabarena_results,
-    ], ignore_index=True)
-
+    print(f"New Configs Hyperparameters: {end_to_end.repo.configs_hyperparameters()}")
     with pd.option_context("display.max_rows", None, "display.max_columns", None, "display.width", 1000):
-        print(f"Results:\n{metrics.head(100)}")
+        print(f"Results:\n{end_to_end_results.model_results.head(100)}")
 
-    calibration_framework = "RF (default)"
-
-    from tabrepo.tabarena.tabarena import TabArena
-    tabarena = TabArena(
-        method_col="method",
-        task_col="dataset",
-        seed_column="fold",
-        error_col="metric_error",
-        columns_to_agg_extra=[
-            "time_train_s",
-            "time_infer_s",
-        ],
-        groupby_columns=[
-            "metric",
-            "problem_type",
-        ]
+    leaderboard: pd.DataFrame = end_to_end_results.compare_on_tabarena(
+        output_dir=eval_dir,
+        filter_dataset_fold=True,  # True: only compare on tasks ran in `results_lst`
+        use_model_results=True,  # If False: Will instead use the ensemble/HPO results
     )
-
-    leaderboard = tabarena.leaderboard(
-        data=metrics,
-        include_elo=True,
-        elo_kwargs={
-            "calibration_framework": calibration_framework,
-        },
-    )
-
-    with pd.option_context("display.max_rows", None, "display.max_columns", None, "display.width", 1000):
-        print(leaderboard)
+    leaderboard_website = format_leaderboard(df_leaderboard=leaderboard)
+    print(leaderboard_website.to_markdown(index=False))
