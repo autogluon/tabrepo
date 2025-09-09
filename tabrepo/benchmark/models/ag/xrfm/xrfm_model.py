865756b82c5e2482ee6047f3bb4b912354451c8d
LennartPurucker <contact@lennart-purucker.com>
2025-09-09 15:20:29 +0200

    maint: try to fix for VRAM problems

--- tabrepo/benchmark/models/ag/xrfm/xrfm_model.py
+++ tabrepo/benchmark/models/ag/xrfm/xrfm_model.py
@@ -16,6 +16,7 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 logger = logging.getLogger(__name__)
 
+
 @contextmanager
 def set_logger_level(logger_name: str, level: int):
     _logger = logging.getLogger(logger_name)
@@ -28,7 +29,6 @@ def set_logger_level(logger_name: str, level: int):
 
 
 class xRFMImplementation:
-
     def __init__(self, problem_type, **kwargs):
         self.problem_type = problem_type
         self.kwargs = kwargs
@@ -38,11 +38,19 @@ class xRFMImplementation:
         import xrfm
 
         # preprocessing
-        self.cat_cols_ = X.select_dtypes(include=["category", "string", "object"]).columns.tolist()
-        self.num_cols_ = X.select_dtypes(exclude=["category", "string", "object"]).columns.tolist()
+        self.cat_cols_ = X.select_dtypes(
+            include=["category", "string", "object"]
+        ).columns.tolist()
+        self.num_cols_ = X.select_dtypes(
+            exclude=["category", "string", "object"]
+        ).columns.tolist()
 
         # Initialize encoders
-        self.ohe_ = OneHotEncoder(handle_unknown="ignore", sparse_output=False) if self.cat_cols_ else None
+        self.ohe_ = (
+            OneHotEncoder(handle_unknown="ignore", sparse_output=False)
+            if self.cat_cols_
+            else None
+        )
         self.scaler_ = StandardScaler()
 
         # Encode categorical variables
@@ -61,7 +69,6 @@ class xRFMImplementation:
 
         x_val_num_enc = X_val.loc[:, self.num_cols_].to_numpy().astype(np.float32)
 
-
         if self.kwargs.get("standardize_cats", False):
             X = np.concatenate([x_cat_enc, x_num_enc], axis=1)
             X_val = np.concatenate([x_val_cat_enc, x_val_num_enc], axis=1)
@@ -69,7 +76,7 @@ class xRFMImplementation:
             X_val = self.scaler_.transform(X_val)
         else:
             if len(self.num_cols_) > 0:
-                x_num_enc    = self.scaler_.fit_transform(x_num_enc)
+                x_num_enc = self.scaler_.fit_transform(x_num_enc)
                 x_val_num_enc = self.scaler_.transform(x_val_num_enc)
             X = np.concatenate([x_cat_enc, x_num_enc], axis=1)
             X_val = np.concatenate([x_val_cat_enc, x_val_num_enc], axis=1)
@@ -111,11 +118,15 @@ class xRFMImplementation:
                 if cat_len > self.kwargs.get("max_cardinality_for_one_hot", 100):
                     categorical_indices.append(cat_idxs)
                     if self.kwargs.get("standardize_cats", False):
-                        cat_vectors = (
-                            np.asarray(self.scaler_.scale_)[None, cat_idxs.numpy()] *
-                            (np.eye(cat_len) - np.asarray(self.scaler_.mean_)[None, cat_idxs.numpy()])
+                        cat_vectors = np.asarray(self.scaler_.scale_)[
+                            None, cat_idxs.numpy()
+                        ] * (
+                            np.eye(cat_len)
+                            - np.asarray(self.scaler_.mean_)[None, cat_idxs.numpy()]
                         )
-                        categorical_vectors.append(torch.tensor(cat_vectors, dtype=torch.float32))
+                        categorical_vectors.append(
+                            torch.tensor(cat_vectors, dtype=torch.float32)
+                        )
                     else:
                         categorical_vectors.append(torch.eye(cat_len))
                 else:
@@ -125,7 +136,9 @@ class xRFMImplementation:
             # numerical indices include small-cardinality one-hot columns and then the true numerical block
             numerical_block = idx + torch.arange(x_num_enc.shape[1])
             if len(numerical_indices_parts) > 0:
-                numerical_indices = torch.cat([*numerical_indices_parts, numerical_block])
+                numerical_indices = torch.cat(
+                    [*numerical_indices_parts, numerical_block]
+                )
             else:
                 numerical_indices = numerical_block
             self.categorical_info_ = {
@@ -141,12 +154,7 @@ class xRFMImplementation:
             **self.kwargs,
         )
 
-        self.model.fit(
-            X=X,
-            y=y,
-            X_val=X_val,
-            y_val=y_val
-        )
+        self.model.fit(X=X, y=y, X_val=X_val, y_val=y_val)
 
     def preprocess_transform(self, X):
         # Encode categorical variables using the same strategy as in fit
@@ -178,10 +186,8 @@ class xRFMImplementation:
         return self.model.predict_proba(self.preprocess_transform(X))
 
 
-
 # pip install xrfm
 class xRFMModel(AbstractModel):
-
     ag_key = "XRFM"
     ag_name = "xRFM"
 
@@ -193,6 +199,7 @@ class xRFMModel(AbstractModel):
         self._indicator_columns = None
         self._features_bool = None
         self._bool_to_cat = None
+        self._device = None
 
     def _fit(
         self,
@@ -217,7 +224,7 @@ class xRFMModel(AbstractModel):
                 "Fit specified to use GPU, but CUDA is not available on this machine. "
                 "Please switch to CPU usage instead.",
             )
-
+        self._device = device
         hyp = self._get_model_params()
 
         metric_map = {
@@ -236,8 +243,6 @@ class xRFMModel(AbstractModel):
 
         init_kwargs = copy.copy(hyp)
 
-
-
         if tuning_metric is not None:
             init_kwargs["tuning_metric"] = tuning_metric
 
@@ -245,7 +250,9 @@ class xRFMModel(AbstractModel):
         impute_bool = hyp.pop("impute_bool", True)
 
         init_kwargs["standardize_cats"] = init_kwargs.get("standardize_cats", False)
-        init_kwargs["classification_mode"] = init_kwargs.get("classification_mode", "prevalence")
+        init_kwargs["classification_mode"] = init_kwargs.get(
+            "classification_mode", "prevalence"
+        )
 
         solver = init_kwargs.get("solver", "solve")
         if self.problem_type in (REGRESSION, MULTICLASS):
@@ -258,12 +265,15 @@ class xRFMModel(AbstractModel):
         init_kwargs["classification_mode"] = classification_mode
 
         exponent = init_kwargs.get("exponent", 1.0)
+
+        # todo: adjust this dynamically and factor it into the VRAM estimate?
+        batch_size = min(init_kwargs.get("M_batch_size", len(X)), 5_000)
         init_kwargs["rfm_params"] = {
             "model": {
                 "kernel": init_kwargs.get("kernel", "l2"),
                 "bandwidth": init_kwargs.get("bandwidth", 10.0),
                 "exponent": exponent,
-                "norm_p": exponent + (2-exponent) * init_kwargs.get("p_interp", 1.0),
+                "norm_p": exponent + (2 - exponent) * init_kwargs.get("p_interp", 1.0),
                 "diag": init_kwargs.get("diag", True),
                 "bandwidth_mode": init_kwargs.get("bandwidth_mode", "constant"),
                 "fast_categorical": init_kwargs.get("fast_categorical", True),
@@ -271,32 +281,35 @@ class xRFMModel(AbstractModel):
             "fit": {
                 "reg": init_kwargs.get("reg", 1e-3),
                 "iters": init_kwargs.get("iters", 5),
-                "M_batch_size": min(init_kwargs.get("M_batch_size", len(X)), 30_000),  # todo: adjust this dynamically?
+                "M_batch_size": batch_size,
                 "verbose": True,
                 "early_stop_rfm": init_kwargs.get("early_stop_rfm", True),
                 "early_stop_multiplier": init_kwargs.get("early_stop_multiplier", 1.05),
                 "solver": solver,
-            }
+            },
         }
 
         # todo: should we set default_rfm_params as well? -> I guess not?
         # todo: set M_batch_size?
         # todo: do we already need to move stuff to the correct device?
 
-        X = self.preprocess(X, is_train=True, bool_to_cat=bool_to_cat, impute_bool=impute_bool)
+        X = self.preprocess(
+            X, is_train=True, bool_to_cat=bool_to_cat, impute_bool=impute_bool
+        )
 
         if X_val is not None:
             X_val = self.preprocess(X_val)
 
         self.model = xRFMImplementation(
             problem_type=self.problem_type,
-            n_threads=num_cpus,
+            n_threads=num_cpus if num_gpus == 0 else 1,  # avoid VRAM leak
             device=device,
             random_state=self.random_seed,
-            time_limit_s=time_limit - (time.time() - start_time) if time_limit is not None else None,
+            time_limit_s=time_limit - (time.time() - start_time)
+            if time_limit is not None
+            else None,
             **init_kwargs,
         )
-
 
         self.model.fit(
             X=X,
@@ -310,7 +323,14 @@ class xRFMModel(AbstractModel):
 
     # TODO: Move missing indicator + mean fill to a generic preprocess flag available to all models
     # FIXME: bool_to_cat is a hack: Maybe move to abstract model?
-    def _preprocess(self, X: pd.DataFrame, is_train: bool = False, bool_to_cat: bool = False, impute_bool: bool = True, **kwargs) -> np.ndarray:
+    def _preprocess(
+        self,
+        X: pd.DataFrame,
+        is_train: bool = False,
+        bool_to_cat: bool = False,
+        impute_bool: bool = True,
+        **kwargs,
+    ) -> np.ndarray:
         """Imputes missing values via the mean and adds indicator columns for numerical features.
         Converts indicator columns to categorical features to avoid them being treated as numerical by RealMLP.
         """
@@ -320,24 +340,44 @@ class xRFMModel(AbstractModel):
         X = X.copy(deep=True)
         if is_train:
             self._bool_to_cat = bool_to_cat
-            self._features_bool = self._feature_metadata.get_features(required_special_types=["bool"])
+            self._features_bool = self._feature_metadata.get_features(
+                required_special_types=["bool"]
+            )
             if impute_bool:  # Technically this should do nothing useful because bools will never have NaN
-                self._features_to_impute = self._feature_metadata.get_features(valid_raw_types=["int", "float"])
-                self._features_to_keep = self._feature_metadata.get_features(invalid_raw_types=["int", "float"])
+                self._features_to_impute = self._feature_metadata.get_features(
+                    valid_raw_types=["int", "float"]
+                )
+                self._features_to_keep = self._feature_metadata.get_features(
+                    invalid_raw_types=["int", "float"]
+                )
             else:
-                self._features_to_impute = self._feature_metadata.get_features(valid_raw_types=["int", "float"], invalid_special_types=["bool"])
-                self._features_to_keep = [f for f in self._feature_metadata.get_features() if f not in self._features_to_impute]
+                self._features_to_impute = self._feature_metadata.get_features(
+                    valid_raw_types=["int", "float"], invalid_special_types=["bool"]
+                )
+                self._features_to_keep = [
+                    f
+                    for f in self._feature_metadata.get_features()
+                    if f not in self._features_to_impute
+                ]
             if self._features_to_impute:
                 self._imputer = SimpleImputer(strategy="mean", add_indicator=True)
                 self._imputer.fit(X=X[self._features_to_impute])
-                self._indicator_columns = [c for c in self._imputer.get_feature_names_out() if c not in self._features_to_impute]
+                self._indicator_columns = [
+                    c
+                    for c in self._imputer.get_feature_names_out()
+                    if c not in self._features_to_impute
+                ]
         if self._imputer is not None:
             X_impute = self._imputer.transform(X=X[self._features_to_impute])
-            X_impute = pd.DataFrame(X_impute, index=X.index, columns=self._imputer.get_feature_names_out())
+            X_impute = pd.DataFrame(
+                X_impute, index=X.index, columns=self._imputer.get_feature_names_out()
+            )
             if self._indicator_columns:
                 # FIXME: Use CategoryFeatureGenerator? Or tell the model which is category
                 # TODO: Add to features_bool?
-                X_impute[self._indicator_columns] = X_impute[self._indicator_columns].astype("category")
+                X_impute[self._indicator_columns] = X_impute[
+                    self._indicator_columns
+                ].astype("category")
             X = pd.concat([X[self._features_to_keep], X_impute], axis=1)
         if self._bool_to_cat and self._features_bool:
             # FIXME: Use CategoryFeatureGenerator? Or tell the model which is category
@@ -345,7 +385,9 @@ class xRFMModel(AbstractModel):
 
         return X
 
-    def _get_random_seed_from_hyperparameters(self, hyperparameters: dict) -> int | None | str:
+    def _get_random_seed_from_hyperparameters(
+        self, hyperparameters: dict
+    ) -> int | None | str:
         return hyperparameters.get("random_state", "N/A")
 
     def _set_default_params(self):
@@ -373,9 +415,24 @@ class xRFMModel(AbstractModel):
 
         return num_cpus, num_gpus
 
+    def _validate_fit_memory_usage(
+        self,
+        mem_error_threshold: float = 1.0,
+        **kwargs,
+    ) -> tuple[int | None, int | None]:
+        return super()._validate_fit_memory_usage(
+            mem_error_threshold=mem_error_threshold, **kwargs
+        )
+
     def _estimate_memory_usage(self, X: pd.DataFrame, **kwargs) -> int:
         hyperparameters = self._get_model_params()
-        return self.estimate_memory_usage_static(X=X, problem_type=self.problem_type, num_classes=self.num_classes, hyperparameters=hyperparameters, **kwargs)
+        return self.estimate_memory_usage_static(
+            X=X,
+            problem_type=self.problem_type,
+            num_classes=self.num_classes,
+            hyperparameters=hyperparameters,
+            **kwargs,
+        )
 
     @classmethod
     def _estimate_memory_usage_static(
@@ -396,15 +453,23 @@ class xRFMModel(AbstractModel):
         num_samples = len(X)
 
         # taken from pytabkit
-        columns_mem_est = 6e4 * num_samples + 2e6 * num_features + 30.0 * num_samples**2 + 30.0 * num_samples * num_features
+        columns_mem_est = (
+            6e4 * num_samples
+            + 2e6 * num_features
+            + 30.0 * num_samples**2
+            + 30.0 * num_samples * num_features
+        )
 
-        dataset_size_mem_est = 5 * get_approximate_df_mem_usage(X).sum()  # roughly 5x DataFrame memory size
+        dataset_size_mem_est = (
+            5 * get_approximate_df_mem_usage(X).sum()
+        )  # roughly 5x DataFrame memory size
         baseline_overhead_mem_est = 2e8  # 200 MB generic overhead
 
         model_mem_estimate = columns_mem_est + baseline_overhead_mem_est
-        model_mem_estimate = min(model_mem_estimate, 3.8e10)  # using the tree strategy caps at <40 GB
+        model_mem_estimate = min(
+            model_mem_estimate, 3.8e10
+        )  # using the tree strategy caps at <40 GB
         return model_mem_estimate + dataset_size_mem_est
-
 
     @classmethod
     def _class_tags(cls) -> dict:
@@ -414,3 +479,14 @@ class xRFMModel(AbstractModel):
         # TODO: Need to add train params support, track best epoch
         #  How to mirror RealMLP learning rate scheduler while forcing stopping at a specific epoch?
         return {"can_refit_full": False}
+
+    def predict_proba(self, **kwargs) -> np.ndarray:
+        y_pred_proba = super().predict_proba(**kwargs)
+
+        if self._device == "cuda":
+            # backup free up VRAM after prediction
+            import torch
+
+            torch.cuda.empty_cache()
+
+        return y_pred_proba
