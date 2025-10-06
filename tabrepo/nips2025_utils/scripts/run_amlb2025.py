a13674ffb78c5a243120a45fe05687a61775c701
ubuntu <innixma@gmail.com>
2025-10-06 19:02:16 +0000

    Remove unused scripts

--- tabrepo/nips2025_utils/scripts/run_amlb2025.py
+++ tabrepo/nips2025_utils/scripts/run_amlb2025.py
@@ -13,8 +13,6 @@ if __name__ == '__main__':
         "amlb_all.csv"
     ]
 
-    # results_files = ["s3://tabarena/benchmarks/amlb2025/amlb_all.csv"]
-
     results_files = [f"{amlb2025_prefix}{result_file}" for result_file in results_files]
 
     results_dfs = [
@@ -57,19 +55,23 @@ if __name__ == '__main__':
         f"autosklearn2{method_suffix}",
     ]
 
-    # df_results = df_results[df_results["type"] == "multiclass"]
-
     df_results = df_results[(df_results["framework"].str.contains(method_suffix)) | (df_results["framework"].isin(default_methods))]
     df_results = df_results[~df_results["framework"].isin(banned_methods)]
     df_results = df_results[df_results["framework"] != fillna_method]
 
+    df_results = df_results.rename(columns={
+        "framework": "method",
+        "training_duration": "time_train_s",
+        "predict_duration": "time_infer_s",
+    })
+
     arena = TabArena(
-        method_col="framework",
+        method_col="method",
         task_col="task",
         error_col="metric_error",
         columns_to_agg_extra=[
-            "training_duration",
-            "predict_duration",
+            "time_train_s",
+            "time_infer_s",
         ],
         seed_column="fold",
     )
@@ -79,7 +81,7 @@ if __name__ == '__main__':
     df_fillna = None
     df_results_fillna = arena.fillna_data(data=df_results, df_fillna=df_fillna, fillna_method="worst")
 
-    results_agg = arena.leaderboard(
+    leaderboard = arena.leaderboard(
         data=df_results_fillna,
         # include_error=True,
         include_elo=True,
@@ -95,29 +97,5 @@ if __name__ == '__main__':
         baseline_relative_error=baseline_method,
         relative_error_kwargs={"agg": "gmean"},
     )
-    print(results_agg)
-
-    results_per_task = arena.compute_results_per_task(data=df_results_fillna)
-    arena.plot_critical_diagrams(results_per_task=results_per_task, save_path=f"./figures/amlb2025/critical-diagram.png")
-    arena.plot_critical_diagrams(results_per_task=results_per_task, save_path=f"./figures/amlb2025/critical-diagram.pdf")
-
-    results_per_task_rename = results_per_task.rename(columns={
-        arena.method_col: "framework",
-        arena.task_col: "dataset",
-        "training_duration": "time_train_s",
-        "predict_duration": "time_infer_s",
-        arena.error_col: "metric_error",
-        "improvability": "bestdiff",
-    })
-
-    from autogluon_benchmark.plotting.plotter import Plotter
-    plotter = Plotter(
-        results_ranked_df=results_per_task_rename,
-        results_ranked_fillna_df=results_per_task_rename,
-        save_dir=f"./figures/amlb2025"
-    )
-
-    plotter.plot_all(
-        calibration_framework=baseline_method,
-        calibration_elo=1000,
-    )
+    with pd.option_context("display.max_rows", None, "display.max_columns", None, "display.width", 1000):
+        print(leaderboard)
